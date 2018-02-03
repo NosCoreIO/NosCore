f93a276783b0ae348576079f910d587f3d0c6aca
0Lucifer0 <erwan.joly@hotmail.fr>
2018-02-03 20:07:38 +1300

    use repository structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenNosCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
