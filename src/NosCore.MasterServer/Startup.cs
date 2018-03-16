82c4ab8769565e8e5ad8ed20d5bfbe47f55f44d0
0Lucifer0 <erwan.joly@hotmail.fr>
2018-03-16 20:03:39 +1300

    use swagger fix #15

--- src/NosCore.MasterServer/Startup.cs
+++ src/NosCore.MasterServer/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,10 +13,21 @@ namespace NosCore.MasterServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "NosCore Master API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "NosCore Master API");
+            });
+
             app.UseMvc();
         }
     }
--- src/NosCore.WorldServer/Startup.cs
+++ src/NosCore.WorldServer/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NosCore.Configuration;
 using NosCore.Core.Logger;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace NosCore.WorldServer
 {
@@ -12,10 +13,21 @@ namespace NosCore.WorldServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "NosCore World API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "NosCore World API");
+            });
+
             app.UseMvc();
         }
     }
