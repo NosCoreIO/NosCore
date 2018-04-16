f4a1bbddc925a15387e320227591aa847bca6a3f
Zagith <geogrimaldi@icloud.com>
2018-04-16 13:35:39 +0200

    Add Broadcast system

---
+++ src/NosCore.GameObject/Networking/BroadcastPacket.cs
@@ -0,0 +1,40 @@
+﻿using NosCore.Domain.Interaction;
+
+namespace NosCore.GameObject.Networking
+{
+    public class BroadcastPacket
+    {
+        #region Instantiation
+
+        public BroadcastPacket(ClientSession session, string packet, ReceiverType receiver, string someonesCharacterName = "", long someonesCharacterId = -1, int xCoordinate = 0, int yCoordinate = 0)
+        {
+            Sender = session;
+            Packet = packet;
+            Receiver = receiver;
+            SomeonesCharacterName = someonesCharacterName;
+            SomeonesCharacterId = someonesCharacterId;
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Packet { get; set; }
+
+        public ReceiverType Receiver { get; set; }
+
+        public ClientSession Sender { get; set; }
+
+        public long SomeonesCharacterId { get; set; }
+
+        public string SomeonesCharacterName { get; set; }
+
+        public int XCoordinate { get; set; }
+
+        public int YCoordinate { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/NosCore.GameObject/Networking/BroadcastableBase.cs
@@ -0,0 +1,88 @@
+﻿using NosCore.Core.Logger;
+using NosCore.Core.Serializing;
+using NosCore.Domain.Interaction;
+using System;
+using System.Collections.Concurrent;
+
+namespace NosCore.GameObject.Networking
+{
+    public abstract class BroadcastableBase
+    {
+        #region Members
+
+        /// <summary>
+        /// List of all connected clients.
+        /// </summary>
+        private readonly ConcurrentDictionary<long, ClientSession> _sessions;
+
+        #endregion
+
+        #region Instantiation
+
+        protected BroadcastableBase()
+        {
+
+            _sessions = new ConcurrentDictionary<long, ClientSession>();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void Broadcast(string packet)
+        {
+            Broadcast(null, packet);
+        }
+
+        public void Broadcast(string packet, int xRangeCoordinate, int yRangeCoordinate)
+        {
+            Broadcast(new BroadcastPacket(null, packet, ReceiverType.AllInRange, xCoordinate: xRangeCoordinate, yCoordinate: yRangeCoordinate));
+        }
+
+        public void Broadcast(PacketDefinition packet)
+        {
+            Broadcast(null, packet);
+        }
+
+        public void Broadcast(PacketDefinition packet, int xRangeCoordinate, int yRangeCoordinate)
+        {
+            Broadcast(new BroadcastPacket(null, PacketFactory.Serialize(packet), ReceiverType.AllInRange, xCoordinate: xRangeCoordinate, yCoordinate: yRangeCoordinate));
+        }
+
+        public void Broadcast(ClientSession client, PacketDefinition packet, ReceiverType receiver = ReceiverType.All, string characterName = "", long characterId = -1)
+        {
+            Broadcast(client, PacketFactory.Serialize(packet), receiver, characterName, characterId);
+        }
+
+        public void Broadcast(BroadcastPacket packet)
+        {
+            try
+            {
+                SpreadBroadcastpacket(packet);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        public void Broadcast(ClientSession client, string content, ReceiverType receiver = ReceiverType.All, string characterName = "", long characterId = -1)
+        {
+            try
+            {
+                SpreadBroadcastpacket(new BroadcastPacket(client, content, receiver, characterName, characterId));
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        private void SpreadBroadcastpacket(BroadcastPacket sentPacket)
+        {
+            // TODO
+        }
+        #endregion
+    }
+}
