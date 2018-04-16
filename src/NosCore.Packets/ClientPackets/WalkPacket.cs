6643386d910b68a94649fd8cc3f0d6aecab4a135
Zagith <geogrimaldi@icloud.com>
2018-04-16 13:44:09 +0200

    Implement Walk Packet

---
+++ src/NosCore.Packets/ClientPackets/WalkPacket.cs
@@ -0,0 +1,24 @@
+﻿using NosCore.Core.Serializing;
+
+namespace NosCore.GameObject
+{
+    [PacketHeader("walk")]
+    public class WalkPacket : PacketDefinition
+    {
+        #region Properties
+
+        [PacketIndex(0)]
+        public short XCoordinate { get; set; }
+
+        [PacketIndex(1)]
+        public short YCoordinate { get; set; }
+
+        [PacketIndex(2)]
+        public short Unknown { get; set; }
+
+        [PacketIndex(3)]
+        public short Speed { get; set; }
+
+        #endregion
+    }
+}
