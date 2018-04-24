2b211c357600ae6cc02fd4f12732664788484b09
Fizo <Administrateur@VPS-97584>
2018-04-24 19:56:38 +0200

    [ADD] $Speed command

---
+++ src/NosCore.Handler/CommandPacketHandler.cs
@@ -0,0 +1,41 @@
+﻿using NosCore.GameObject.ComponentEntities.Extensions;
+using NosCore.GameObject.Networking;
+using NosCore.Packets.CommandPackets;
+
+namespace NosCore.Handler
+{
+    public class CommandPacketHandler
+    {
+        #region Members
+
+        #endregion
+
+        #region Instantiation
+        public CommandPacketHandler()
+        { }
+        public CommandPacketHandler(ClientSession session)
+        {
+            Session = session;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ClientSession Session { get; }
+
+        #endregion
+
+        #region Methods
+        public void Speed(SpeedPacket speedPacket)
+        {
+            if (speedPacket.Speed >= 60)
+            {
+                return;
+            }
+            Session.Character.Speed = speedPacket.Speed;
+            Session.SendPacket(Session.Character.GenerateCond());
+        }
+        #endregion
+    }
+}
