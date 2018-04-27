e9165b05a9e01b5f7afd93f730344c7c866f5734
0Lucifer0 <erwan.joly@hotmail.fr>
2018-04-27 22:54:04 +1200

    start adding autofac dependancy injection

--- src/NosCore.WorldServer/WorldServer.cs
+++ src/NosCore.WorldServer/WorldServer.cs
@@ -36,116 +36,26 @@ using NosCore.Core.Serializing.HandlerSerialization;
 using NosCore.Data.AliveEntities;
 using NosCore.Domain;
 using NosCore.Handler;
+using Autofac;
 
 namespace NosCore.WorldServer
 {
-    public static class WorldServer
+    public class WorldServer
     {
-        private static readonly WorldConfiguration _worldConfiguration = new WorldConfiguration();
+        private readonly List<IPacketHandler> _clientPacketDefinitions;
 
-        private const string _configurationPath = @"..\..\..\configuration";
+        private readonly WorldConfiguration _worldConfiguration;
 
-        private static List<IPacketHandler> _clientPacketDefinitions;
-
-        private static void InitializeConfiguration()
+        public WorldServer(WorldConfiguration worldConfiguration, List<IPacketHandler> clientPacketDefinition)
         {
-            var builder = new ConfigurationBuilder();
-            builder.SetBasePath(Directory.GetCurrentDirectory() + _configurationPath);
-            builder.AddJsonFile("world.json", false);
-            builder.Build().Bind(_worldConfiguration);
-            Logger.Log.Info(LogLanguage.Instance.GetMessageFromKey(LogLanguageKey.SUCCESSFULLY_LOADED));
+            _worldConfiguration = worldConfiguration;
+            _clientPacketDefinitions = clientPacketDefinition;
         }
 
-        private static void InitializeLogger()
+        public void Run()
         {
-            // LOGGER
-            ILoggerRepository logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("../../configuration/log4net.config"));
-            Logger.InitializeLogger(LogManager.GetLogger(typeof(WorldServer)));
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-           WebHost.CreateDefaultBuilder(args)
-               .UseStartup<Startup>()
-               .UseUrls(_worldConfiguration.WebApi.ToString())
-               .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Warning))
-               .PreferHostingUrls(true)
-               .Build();
-
-        private static void InitializePackets()
-        {
-            PacketFactory.Initialize<NoS0575Packet>();
-            _clientPacketDefinitions = typeof(DefaultPacketHandler).Assembly.GetInstancesOfImplementingTypes<IPacketHandler>().ToList();
-        }
-
-        private static void ConnectMaster()
-        {
-            while (true)
-            {
-                try
-                {
-                    RunMasterClient(_worldConfiguration.MasterCommunication.Host, Convert.ToInt32(_worldConfiguration.MasterCommunication.Port), _worldConfiguration.MasterCommunication.Password, new MasterClient() { Name = "WorldServer", Type = ServerType.WorldServer, WebApi = _worldConfiguration.WebApi }, WebApi: _worldConfiguration.WebApi, connectedAccountLimit: _worldConfiguration.ConnectedAccountLimit, clientPort: _worldConfiguration.Port, serverGroup: _worldConfiguration.ServerGroup, serverHost: _worldConfiguration.Host).Wait();
-                    break;
-                }
-                catch
-                {
-                    Logger.Log.Error(LogLanguage.Instance.GetMessageFromKey(LogLanguageKey.MASTER_SERVER_RETRY));
-                    Thread.Sleep(5000);
-                }
-            }
-        }
-
-        private static void PrintHeader()
-        {
-            Console.Title = "NosCore - WorldServer";
-            const string text = "WORLD SERVER - 0Lucifer0";
-            int offset = (Console.WindowWidth / 2) + (text.Length / 2);
-            string separator = new string('=', Console.WindowWidth);
-            Console.WriteLine(separator + string.Format("{0," + offset + "}\n", text) + separator);
-        }
-
-        public static async Task RunMasterClient(string targetHost, int port, string password, MasterClient clientType, ServerConfiguration WebApi, int connectedAccountLimit = 0, int clientPort = 0, byte serverGroup = 0, string serverHost = "")
-        {
-            var group = new MultithreadEventLoopGroup();
-
-            var bootstrap = new Bootstrap();
-            bootstrap
-                .Group(group)
-                .Channel<TcpSocketChannel>()
-                .Option(ChannelOption.TcpNodelay, true)
-                .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
-                {
-                    IChannelPipeline pipeline = channel.Pipeline;
-
-                    pipeline.AddLast(new LengthFieldPrepender(2));
-                    pipeline.AddLast(new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
-
-                    pipeline.AddLast(new StringEncoder(), new StringDecoder());
-                    pipeline.AddLast(new MasterClientSession(password));
-                }));
-            var connection = await bootstrap.ConnectAsync(new IPEndPoint(IPAddress.Parse(targetHost), port)).ConfigureAwait(false);
-
-            await connection.WriteAndFlushAsync(new Channel()
-            {
-                Password = password,
-                ClientName = clientType.Name,
-                ClientType = (byte)clientType.Type,
-                ConnectedAccountsLimit = connectedAccountLimit,
-                Port = clientPort,
-                ServerGroup = serverGroup,
-                Host = serverHost,
-                WebApi = WebApi
-            }).ConfigureAwait(false);
-        }
-
-        public static void Main(string[] args)
-        {
-            PrintHeader();
-            InitializeLogger();
-            InitializeConfiguration();
-            BuildWebHost(args).StartAsync();
+            BuildWebHost(null).StartAsync();
             Mapping.Mapper.InitializeMapping();
-            InitializePackets();
             ConnectMaster();
             if (DataAccessHelper.Instance.Initialize(_worldConfiguration.Database))
             {
@@ -160,5 +70,65 @@ namespace NosCore.WorldServer
                 return;
             }
         }
+
+        private IWebHost BuildWebHost(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>()
+               .UseUrls(_worldConfiguration.WebApi.ToString())
+               .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Warning))
+               .PreferHostingUrls(true)
+               .Build();
+
+        private  void ConnectMaster()
+        {
+            async Task RunMasterClient(string targetHost, int port, string password, MasterClient clientType, ServerConfiguration WebApi, int connectedAccountLimit = 0, int clientPort = 0, byte serverGroup = 0, string serverHost = "")
+            {
+                var group = new MultithreadEventLoopGroup();
+
+                var bootstrap = new Bootstrap();
+                bootstrap
+                    .Group(group)
+                    .Channel<TcpSocketChannel>()
+                    .Option(ChannelOption.TcpNodelay, true)
+                    .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+
+                        pipeline.AddLast(new LengthFieldPrepender(2));
+                        pipeline.AddLast(new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
+
+                        pipeline.AddLast(new StringEncoder(), new StringDecoder());
+                        pipeline.AddLast(new MasterClientSession(password));
+                    }));
+                var connection = await bootstrap.ConnectAsync(new IPEndPoint(IPAddress.Parse(targetHost), port)).ConfigureAwait(false);
+
+                await connection.WriteAndFlushAsync(new Channel()
+                {
+                    Password = password,
+                    ClientName = clientType.Name,
+                    ClientType = (byte)clientType.Type,
+                    ConnectedAccountsLimit = connectedAccountLimit,
+                    Port = clientPort,
+                    ServerGroup = serverGroup,
+                    Host = serverHost,
+                    WebApi = WebApi
+                }).ConfigureAwait(false);
+            }
+
+            while (true)
+            {
+                try
+                {
+                    RunMasterClient(_worldConfiguration.MasterCommunication.Host, Convert.ToInt32(_worldConfiguration.MasterCommunication.Port), _worldConfiguration.MasterCommunication.Password, new MasterClient() { Name = "WorldServer", Type = ServerType.WorldServer, WebApi = _worldConfiguration.WebApi }, WebApi: _worldConfiguration.WebApi, connectedAccountLimit: _worldConfiguration.ConnectedAccountLimit, clientPort: _worldConfiguration.Port, serverGroup: _worldConfiguration.ServerGroup, serverHost: _worldConfiguration.Host).Wait();
+                    break;
+                }
+                catch
+                {
+                    Logger.Log.Error(LogLanguage.Instance.GetMessageFromKey(LogLanguageKey.MASTER_SERVER_RETRY));
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+        
     }
 }
