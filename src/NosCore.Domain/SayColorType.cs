dc6f5502e7e78dc27a97154ddfd870fb50577192
Zagith <geogrimaldi@icloud.com>
2018-05-09 14:39:02 +0200

    [ADD] enum for SayPacket color #74

---
+++ src/NosCore.Domain/SayColorType.cs
@@ -0,0 +1,14 @@
+﻿namespace NosCore.Domain
+{
+    public enum SayColorType
+    {
+        DarkPurple = 0,
+        LightGreen = 2,
+        Red = 3,
+        LightYellow = 6,
+        Yellow = 10,
+        Purple = 11,
+        Green = 12,
+        Grey = 13
+    }
+}
