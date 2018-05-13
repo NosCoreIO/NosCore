93059f34c450fbb49880a151b55907bbbe0ad960
0Lucifer0 <erwan.joly@hotmail.fr>
2018-05-13 17:32:10 +1200

    fix enum

--- src/NosCore.Domain/SayColorType.cs
+++ src/NosCore.Domain/SayColorType.cs
@@ -2,16 +2,15 @@
 {
     public enum SayColorType
     {
-        DarkPurple = 0,
+        White = 0,
         LightGreen = 3,
         DarkGrey = 4,
-        Red = 5,
+        RedWhisper = 5,
         Blue = 6,
         LightYellow = 7,
+        RedW = 8,
         Yellow = 10,
         Purple = 11,
         Green = 12,
-        White = 16
-
     }
 }
