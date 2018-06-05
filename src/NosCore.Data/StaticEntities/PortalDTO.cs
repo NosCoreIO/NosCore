d01a0dbfd5dcd0b2ea4046b963f6015551555664
Zagith <geogrimaldi@icloud.com>
2018-06-05 18:27:51 +0200

    Add PortalDTO

---
+++ src/NosCore.Data/StaticEntities/PortalDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NosCore.Data.StaticEntities
+{
+    public class PortalDTO : IDTO
+    {
+        public short DestinationMapId { get; set; }
+
+        public short DestinationX { get; set; }
+
+        public short DestinationY { get; set; }
+
+        public bool IsDisabled { get; set; }
+
+        public int PortalId { get; set; }
+
+        public short SourceMapId { get; set; }
+
+        public short SourceX { get; set; }
+
+        public short SourceY { get; set; }
+
+        public short Type { get; set; }
+    }
+}
---
+++ src/NosCore.Parser/Parsers/PortalParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosCore.Parser.Parsers
+{
+    class PortalParser
+    {
+        public void ParsePortals()
+        {
+
+        }
+    }
+}
