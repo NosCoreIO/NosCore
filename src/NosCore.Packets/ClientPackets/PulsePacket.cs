b289ee7ccf9f93e4cab4d2af8e1c212b82761c1f
Fizo55 <hugomacees56000@gmail.com>
2018-06-23 17:56:57 +0200

    [REMOVE] region

--- src/NosCore.Packets/ClientPackets/PulsePacket.cs
+++ src/NosCore.Packets/ClientPackets/PulsePacket.cs
@@ -5,11 +5,7 @@ namespace NosCore.Packets.ClientPackets
     [PacketHeader("pulse")]
     public class PulsePacket
     {
-        #region Properties
-
         [PacketIndex(0)]
         public int Tick { get; set; }
-
-        #endregion
     }
 }
