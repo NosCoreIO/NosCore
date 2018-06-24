71459939971bdc454bf3eeaa8037552cfe7fb1ae
Fizo55 <hugomacees56000@gmail.com>
2018-06-24 20:15:23 +0200

    [REMOVE] region && change the name of the parameters

--- src/NosCore.Packets/ServerPackets/StatPacket.cs
+++ src/NosCore.Packets/ServerPackets/StatPacket.cs
@@ -8,26 +8,22 @@ namespace NosCore.Packets.ServerPackets
     [PacketHeader("stat")]
     public class StatPacket : PacketDefinition
     {
-        #region Properties
-
         [PacketIndex(0)]
         public int HP { get; set; }
 
         [PacketIndex(1)]
-        public double HPLoad { get; set; }
+        public double HPMaximum { get; set; }
 
         [PacketIndex(2)]
         public int MP { get; set; }
 
         [PacketIndex(3)]
-        public double MPLoad { get; set; }
+        public double MPMaximum { get; set; }
 
         [PacketIndex(4)]
         public int Unknown { get; set; }
 
         [PacketIndex(5)]
         public double Option { get; set; }
-
-        #endregion
     }
 }
