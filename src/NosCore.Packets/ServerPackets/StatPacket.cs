2d39424526c4d36a2a0bf436c8c58215f0b8bc44
Fizo55 <hugomacees56000@gmail.com>
2018-06-28 23:36:21 +0200

    Finish add stat packet

---
+++ src/NosCore.Packets/ServerPackets/StatPacket.cs
@@ -0,0 +1,29 @@
+using NosCore.Core.Serializing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosCore.Packets.ServerPackets
+{
+    [PacketHeader("stat")]
+    public class StatPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public int HP { get; set; }
+
+        [PacketIndex(1)]
+        public double HPMaximum { get; set; }
+
+        [PacketIndex(2)]
+        public int MP { get; set; }
+
+        [PacketIndex(3)]
+        public double MPMaximum { get; set; }
+
+        [PacketIndex(4)]
+        public int Unknown { get; set; }//TODO to find
+
+        [PacketIndex(5)]
+        public double Option { get; set; }
+    }
+}
