fb79674a44778a45201e6a228c13d70371947f05
Elendan <arthur.despeaux@hotmail.fr>
2018-07-17 00:14:03 +0200

    Remove todo

--- src/NosCore.Core/Networking/NetworkClient.cs
+++ src/NosCore.Core/Networking/NetworkClient.cs
@@ -78,7 +78,6 @@ namespace NosCore.Core.Networking
 			_channel?.Flush();
 		}
 
-        //Todo: delete this later, it's just for testing purposes
 	    public void SendPacket(string packet)
 	    {
 	        if (packet == null)
