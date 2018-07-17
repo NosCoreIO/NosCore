43ec9f497908b83167413a4b577c73c94aa6bbbf
Elendan <arthur.despeaux@hotmail.fr>
2018-07-17 02:33:24 +0200

    Add $Shout command

---
+++ src/NosCore.Packets/CommandPackets/ShoutPacket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Account;
+
+namespace NosCore.Packets.CommandPackets
+{
+    [PacketHeader("$Shout", Authority = AuthorityType.GameMaster)]
+    public class ShoutPacket : PacketDefinition
+    {
+        [PacketIndex(0, SerializeToEnd = true)]
+        public string Message { get; set; }
+    }
+}
