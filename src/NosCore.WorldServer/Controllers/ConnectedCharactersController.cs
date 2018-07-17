ea955c5a2857c1b63c6b43e0165bebc928c773cc
Elendan <arthur.despeaux@hotmail.fr>
2018-07-17 15:01:57 +0200

    Finish chat system

---
+++ src/NosCore.WorldServer/Controllers/ConnectedCharactersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NosCore.Core;
+using NosCore.GameObject.Networking;
+using NosCore.Shared.Enumerations.Account;
+
+namespace NosCore.WorldServer.Controllers
+{
+    [Route("api/[controller]")]
+    class ConnectedCharactersController
+    {
+        // GET api/connectedCharacters
+        [HttpGet]
+        [AllowAnonymous]
+        public IEnumerable<string> GetConnectedCharacters()
+        {
+            return ServerManager.Instance.Sessions.Select(s => s.Value.Character.Name);
+        }
+    }
+}
