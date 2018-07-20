fa311012e40a905fe9fc450e2e63ad4067fe1e5e
Elendan <arthur.despeaux@hotmail.fr>
2018-07-20 17:46:54 +0200

    Add blacklist add & delete

---
+++ src/NosCore.Packets/ClientPackets/BlInsPacket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Core.Serializing;
+
+namespace NosCore.Packets.ClientPackets
+{
+    [PacketHeader("blins")]
+    public class BlInsPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public long CharacterId { get; set; }
+    }
+}
---
+++ src/NosCore.Packets/ServerPackets/BlinitPacket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Core.Serializing;
+
+namespace NosCore.Packets.ServerPackets
+{
+    [PacketHeader("blinit")]
+    public class BlinitPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public List<BlinitSubPacket> SubPackets { get; set; }
+    }
+}
