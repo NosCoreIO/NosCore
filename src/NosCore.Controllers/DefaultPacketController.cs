f9118f155570de59297c8eb6b8c9f65d5507cffa
Elendan <arthur.despeaux@hotmail.fr>
2018-07-21 17:58:18 +0200

    Update nested condition

--- src/NosCore.Controllers/DefaultPacketController.cs
+++ src/NosCore.Controllers/DefaultPacketController.cs
@@ -358,17 +358,18 @@ namespace NosCore.Controllers
                     ServerManager.Instance.Sessions.Values.FirstOrDefault(s => s.Character?.Name == receiverName);
                 if (receiverSession != null)
                 {
-                    if (receiverSession.Character.CharacterRelations.Values.Any(s => s.RelatedCharacterId == Session.Character.CharacterId && s.RelationType == CharacterRelationType.Blocked))
+                    receiverSession.SendPacket(speakPacket);
+
+                    if (!receiverSession.Character.CharacterRelations.Values.Any(s => s.RelatedCharacterId == Session.Character.CharacterId && s.RelationType == CharacterRelationType.Blocked))
                     {
-                        receiverSession.SendPacket(new SayPacket
-                        {
-                            Message = Language.Instance.GetMessageFromKey(LanguageKey.BLACKLIST_BLOCKED, Session.Account.Language),
-                            Type = SayColorType.Yellow
-                        });
                         return;
                     }
 
-                    receiverSession?.SendPacket(speakPacket);
+                    receiverSession.SendPacket(new SayPacket
+                    {
+                        Message = Language.Instance.GetMessageFromKey(LanguageKey.BLACKLIST_BLOCKED, Session.Account.Language),
+                        Type = SayColorType.Yellow
+                    });
                     return;
                 }
 
