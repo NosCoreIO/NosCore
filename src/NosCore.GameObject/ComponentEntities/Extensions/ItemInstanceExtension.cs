45bd43d26bfa7ea31358eb30c3ea432f7b940d87
0Lucifer0 <erwan.joly@gmail.com>
2018-07-30 21:19:30 +1200

    fix build and disable methode for easier implementation

---
+++ src/NosCore.GameObject/ComponentEntities/Extensions/ItemInstanceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.GameObject.Item;
+using NosCore.Shared.Enumerations.Items;
+
+namespace NosCore.GameObject.ComponentEntities.Extensions
+{
+    public static class ItemInstanceExtension
+    {
+        public static IvnPacket GeneratePocketChange(this ItemInstance itemInstance, PocketType type, short slot)
+        {
+            if (itemInstance == null)
+            {
+                return new IvnPacket
+                {
+                    Type = type,
+                    Slot = slot,
+                    VNum = -1,
+                    Rare = 0,
+                    Upgrade = 0,
+                    SecondUpgrade = 0
+                };
+            }
+        }
+    }
+}
