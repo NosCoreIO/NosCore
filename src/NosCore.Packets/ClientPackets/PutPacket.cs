84baf0c63f21a859b8e41ead83b4b6a57e46db3c
0Lucifer0 <erwan.joly@gmail.com>
2018-08-05 16:14:23 +1200

    add put item

---
+++ src/NosCore.Packets/ClientPackets/PutPacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Items;
+
+namespace NosCore.Packets.ClientPackets
+{
+    [PacketHeader("put")]
+    public class PutPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public PocketType PocketType { get; set; }
+
+        [PacketIndex(1)]
+        public byte Slot { get; set; }
+
+        [PacketIndex(2)]
+        public short Amount { get; set; } 
+    }
+}
