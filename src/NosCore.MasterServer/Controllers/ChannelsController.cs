e37acc2736767586a6d32a73a8d766097360fcba
0Lucifer0 <erwan.joly@gmail.com>
2018-09-02 14:32:50 +1200

    fix optimise api call fix #157

--- src/NosCore.MasterServer/Controllers/ChannelsController.cs
+++ src/NosCore.MasterServer/Controllers/ChannelsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NosCore.Core;
 using NosCore.Core.Networking;
@@ -10,8 +11,11 @@ namespace NosCore.MasterServer.Controllers
     {
         // GET api/channels
         [HttpGet]
-        public List<WorldServerInfo> GetChannels()
+        public List<WorldServerInfo> GetChannels(long? id)
         {
+            if (id != null) {
+                return MasterClientListSingleton.Instance.WorldServers.Where(s=>s.Id == id).ToList();
+            }
             return MasterClientListSingleton.Instance.WorldServers;
         }
     }
