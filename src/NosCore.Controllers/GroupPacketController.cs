e4cfd6020964cccb790f2a36c1d39f8390d9ea36
Elendan <arthur.despeaux@hotmail.fr>
2018-09-06 15:37:02 +0100

    Continue Group system

---
+++ src/NosCore.Controllers/GroupPacketController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+using NosCore.Configuration;
+using NosCore.GameObject;
+using NosCore.GameObject.Networking;
+using NosCore.Packets.ServerPackets;
+using NosCore.Shared.Enumerations.Character;
+using NosCore.Shared.Enumerations.Group;
+using NosCore.Shared.I18N;
+
+namespace NosCore.Controllers
+{
+    public class GroupPacketController : PacketController
+    {
+        private readonly WorldConfiguration _worldConfiguration;
+
+        [UsedImplicitly]
+        public GroupPacketController()
+        {
+            
+        }
+
+        public GroupPacketController(WorldConfiguration worldConfiguration)
+        {
+            _worldConfiguration = worldConfiguration;
+        }
+
+        /// <summary>
+        ///     pjoin packet
+        /// </summary>
+        /// <param name="pjoinPacket"></param>
+        public void GroupJoin(PjoinPacket pjoinPacket)
+        {
+            var targetSession = ServerManager.Instance.Sessions.Values.FirstOrDefault(s => s.Character.CharacterId == pjoinPacket.CharacterId);
+
+            if (targetSession == null && pjoinPacket.RequestType != GroupRequestType.Sharing)
+            {
+                return;
+            }
+
+            switch (pjoinPacket.RequestType)
+            {
+
+                case GroupRequestType.Requested:
+                case GroupRequestType.Invited:
+                    if (pjoinPacket.CharacterId <= 0 || targetSession == null || pjoinPacket.CharacterId == Session.Character.CharacterId)
+                    {
+                        return;
+                    }
+
+                    if (targetSession.Character.Group != null && targetSession.Character.Group.IsGroupFull())
+                    {
+                        Session.SendPacket(new InfoPacket
+                        {
+                            Message = Language.Instance.GetMessageFromKey(LanguageKey.GROUP_FULL, Session.Account.Language)
+                        });
+                        return;
+                    }
+
+                    if (targetSession.Character.Group != null && Session.Character.Group != null)
+                    {
+                        Session.SendPacket(new InfoPacket
+                        {
+                            Message = Language.Instance.GetMessageFromKey(LanguageKey.ALREADY_IN_GROUP, Session.Account.Language)
+                        });
+                        return;
+                    }
+
+                    if (Session.Character.IsRelatedToCharacter(pjoinPacket.CharacterId, CharacterRelationType.Blocked))
+                    {
+                        Session.SendPacket(new InfoPacket
+                        {
+                            Message = Language.Instance.GetMessageFromKey(LanguageKey.BLACKLIST_BLOCKED, Session.Account.Language)
+                        });
+                        return;
+                    }
+
+                    if (targetSession.Character.GroupRequestBlocked)
+                    {
+                        Session.SendPacket(new MsgPacket
+                        {
+                            Message = Language.Instance.GetMessageFromKey(LanguageKey.GROUP_BLOCKED, Session.Account.Language)
+                        });
+                        return;
+                    }
+
+                    Session.Character.GroupRequestCharacterIds.Add(pjoinPacket.CharacterId);
+
+                    if (Session.Character.Group == null || Session.Character.Group.Type == GroupType.Group)
+                    {
+                        if (targetSession.Character?.Group == null || targetSession.Character?.Group.Type == GroupType.Group)
+                        {
+                            targetSession.SendPacket(new DlgPacket
+                            {
+                                Question = Language.Instance.GetMessageFromKey(LanguageKey.INVITED_YOU_GROUP, targetSession.Account.Language),
+                                YesPacket = new PjoinPacket { CharacterId = Session.Character.CharacterId, RequestType = GroupRequestType.Accepted },
+                                NoPacket = new PjoinPacket { CharacterId = Session.Character.CharacterId, RequestType = GroupRequestType.Declined }
+                            });
+                        }
+                    }
+
+                    break;
+            }
+        }
+    }
+}
---
+++ src/NosCore.Packets/ServerPackets/PjoinPacket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Group;
+
+namespace NosCore.Packets.ServerPackets
+{
+    [PacketHeader("pjoin")]
+    public class PjoinPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public GroupRequestType RequestType { get; set; }
+
+        [PacketIndex(1)]
+        public long CharacterId { get; set; }
+    }
+}
