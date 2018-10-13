f4c33e17bcf2d23eb2df6561c3d7a876aba552e1
Fizo55 <hugomacees56000@gmail.com>
2018-10-13 18:44:04 +0200

    [ADD] $Gold command

---
+++ src/NosCore.Packets/CommandPackets/GoldCommandPacket.cs
@@ -0,0 +1,12 @@
+﻿using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Account;
+
+namespace NosCore.Packets.CommandPackets
+{
+    [PacketHeader("$Gold", Authority = AuthorityType.GameMaster)]
+    public class GoldCommandPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public long Gold { get; set; }
+    }
+}
