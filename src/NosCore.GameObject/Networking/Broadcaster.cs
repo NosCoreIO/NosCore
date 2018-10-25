e7dbb69d4d0ee8c57c48c8f9dcab60b8f5698d9c
Elendan <arthur.despeaux@hotmail.fr>
2018-10-25 14:04:38 +0200

    Update relation tests

--- src/NosCore.GameObject/Networking/Broadcaster.cs
+++ src/NosCore.GameObject/Networking/Broadcaster.cs
@@ -71,6 +71,7 @@ namespace NosCore.GameObject.Networking
             return (func == null ? ClientSessions.Values.Where(s => s.Character != null).Select(s => s.Character)
                 : ClientSessions.Values.Where(s => s.Character != null).Select(c => c.Character).Where(func));
         }
+
         public ICharacterEntity GetCharacter(Func<ICharacterEntity, bool> func)
         {
             return (func == null ? ClientSessions.Values.FirstOrDefault(s => s.Character != null)?.Character
