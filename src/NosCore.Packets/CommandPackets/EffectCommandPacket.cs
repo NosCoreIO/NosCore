2c938b0be65204848da8b67ae35405a82c373c45
Zagith <geogrimaldi@icloud.com>
2018-10-27 15:42:05 +0200

    [ADD] Effect command

---
+++ src/NosCore.Packets/CommandPackets/EffectCommandPacket.cs
@@ -0,0 +1,20 @@
+﻿using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosCore.Packets.CommandPackets
+{
+    [PacketHeader("$Effect", Authority = AuthorityType.GameMaster)]
+    public class EffectCommandPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public int EffectId { get; set; }
+
+        public static string ReturnHelp()
+        {
+            return "$Effect EFFECTID";
+        }
+    }
+}
