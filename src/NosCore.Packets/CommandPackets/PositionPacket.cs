dee16947a6f97f1173c1908d356fd81cf4033bdf
Zagith <geogrimaldi@icloud.com>
2018-10-27 16:32:09 +0200

    [ADD] Position command

---
+++ src/NosCore.Packets/CommandPackets/PositionPacket.cs
@@ -0,0 +1,17 @@
+﻿using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosCore.Packets.CommandPackets
+{
+    [PacketHeader("$Position", Authority = AuthorityType.GameMaster)]
+    public class PositionPacket : PacketDefinition
+    {
+        public static string ReturnHelp()
+        {
+            return "$Position";
+        }
+    }
+}
