77754cd72924bd2fdbec7d6c4fa33b364cd52d68
Zagith <geogrimaldi@icloud.com>
2018-11-05 23:26:48 +0100

    fix mistake

--- src/NosCore.Packets/ClientPackets/FAuthPacket.cs
+++ src/NosCore.Packets/ClientPackets/FAuthPacket.cs
@@ -18,6 +18,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Family;
 
 namespace NosCore.Packets.ClientPackets
 {
