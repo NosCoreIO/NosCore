4385e0787091be665227c57674cdbcc2268f2586
Zagith <geogrimaldi@icloud.com>
2018-11-05 23:28:50 +0100

    remove usless regions

--- src/NosCore.Packets/ClientPackets/JoinFamilyPacket.cs
+++ src/NosCore.Packets/ClientPackets/JoinFamilyPacket.cs
@@ -24,14 +24,10 @@ namespace NosCore.Packets.ClientPackets
     [PacketHeader("gjoin")]
     public class JoinFamilyPacket : PacketDefinition
     {
-        #region Properties
-
         [PacketIndex(0)]
         public byte Type { get; set; }
 
         [PacketIndex(1)]
         public long CharacterId { get; set; }
-
-        #endregion
     }
 }
