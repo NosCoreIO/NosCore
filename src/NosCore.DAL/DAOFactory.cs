63b14a7256148c1c8b018646d35d1b941092d602
Zagith <geogrimaldi@icloud.com>
2018-11-07 16:01:16 +0100

    implement FamilyCharacterDao - FamilyLogDao and fix mistake

--- src/NosCore.DAL/DAOFactory.cs
+++ src/NosCore.DAL/DAOFactory.cs
@@ -64,6 +64,8 @@ namespace NosCore.DAL
         private static GenericDao<CharacterRelation, CharacterRelationDto> _characterRelationDao;
         private static ItemInstanceDao _itemInstanceDao;
         private static GenericDao<Family, FamilyDto> _familyDao;
+        private static GenericDao<FamilyCharacter, FamilyCharacterDto> _familyCharacterDao;
+        private static GenericDao<FamilyLog, FamilyLogDto> _familyLogDao;
 
         public static GenericDao<Drop, DropDto> DropDao => _dropDao ??
             (_dropDao = new GenericDao<Drop, DropDto>());
@@ -171,5 +173,11 @@ namespace NosCore.DAL
 
         public static GenericDao<Family, FamilyDto> FamilyDao =>
             _familyDao ?? (_familyDao = new GenericDao<Family, FamilyDto>());
+
+        public static GenericDao<FamilyCharacter, FamilyCharacterDto> FamilyCharacterDao =>
+            _familyCharacterDao ?? (_familyCharacterDao = new GenericDao<FamilyCharacter, FamilyCharacterDto>());
+
+        public static GenericDao<FamilyLog, FamilyLogDto> FamilyLogDao =>
+            _familyLogDao ?? (_familyLogDao = new GenericDao<FamilyLog, FamilyLogDto>());
     }
 }
