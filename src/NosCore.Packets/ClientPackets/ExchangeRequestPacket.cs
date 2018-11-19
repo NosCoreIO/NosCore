c7eb992dc3cbf8130c3ff0139dc398906212a663
Elendan <arthur.despeaux@hotmail.fr>
2018-11-19 18:01:54 +0100

    Start exchange system

---
+++ src/NosCore.Packets/ClientPackets/ExchangeRequestPacket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Interaction;
+
+namespace NosCore.Packets.ClientPackets
+{
+    [PacketHeader("req_exc")]
+    public class ExchangeRequestPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public RequestExchangeType RequestType { get; set; }
+
+        [PacketIndex(1)]
+        [Range(0, long.MaxValue)]
+        public long VisualId { get; set; }
+    }
+}
---
+++ src/NosCore.Packets/ServerPackets/ModalPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Core.Serializing;
+
+namespace NosCore.Packets.ServerPackets
+{
+    [PacketHeader("modal")]
+    public class ModalPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public byte Type { get; set; }
+
+        [PacketIndex(1, SerializeToEnd = true)]
+        public string Message { get; set; }
+    }
+}
