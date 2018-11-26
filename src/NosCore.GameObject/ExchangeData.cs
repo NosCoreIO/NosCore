2d26c83a273e44bd48b30f348b586a78dcc5f1d3
Elendan <arthur.despeaux@hotmail.fr>
2018-11-26 14:56:54 +0100

    Start tests on Exchange

--- src/NosCore.GameObject/ExchangeData.cs
+++ src/NosCore.GameObject/ExchangeData.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using NosCore.GameObject.Services.ItemBuilder.Item;
 
 namespace NosCore.GameObject
 {
-    public class ExchangeData
+    public class ExchangeData : ConcurrentDictionary<long, ItemInstance>
     {
         public long TargetVisualId { get; set; }
+
+        public long Gold { get; set; }
+
+        public long BankGold { get; set; }
     }
 }
