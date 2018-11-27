d14e44d51d4affc60cbd00a947ae4005972543c1
Elendan <arthur.despeaux@hotmail.fr>
2018-11-27 01:04:12 +0100

    cleanup code

--- src/NosCore.GameObject/Map/Map.cs
+++ src/NosCore.GameObject/Map/Map.cs
@@ -28,9 +28,35 @@ namespace NosCore.GameObject.Map
 {
     public class Map : MapDto
     {
-        public short XLength => BitConverter.ToInt16(Data.AsSpan().Slice(0, 2).ToArray(), 0);
+        private short _xLength;
 
-        public short YLength => BitConverter.ToInt16(Data.AsSpan().Slice(2, 2).ToArray(), 0);
+        private short _yLength;
+
+        public short XLength
+        {
+            get
+            {
+                if (_xLength == 0)
+                {
+                    _xLength = BitConverter.ToInt16(Data.AsSpan().Slice(0, 2).ToArray(), 0);
+                }
+
+                return _xLength;
+            }
+        }
+
+        public short YLength
+        {
+            get
+            {
+                if (_yLength == 0)
+                {
+                    _yLength = BitConverter.ToInt16(Data.AsSpan().Slice(2, 2).ToArray(), 0);
+                }
+
+                return _yLength;
+            }
+        }
 
         public byte this[short x, short y] => Data.AsSpan().Slice(4 + (y * XLength) + x, 1)[0];
 
