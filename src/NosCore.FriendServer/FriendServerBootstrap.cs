3afd633a89e22ea484a5fb2d515308e63b340819
0Lucifer0 <erwan.joly@gmail.com>
2018-12-03 19:06:17 +1300

    creating friend service fix #313

---
+++ src/NosCore.FriendServer/FriendServerBootstrap.cs
@@ -0,0 +1,56 @@
+﻿//  __  _  __    __   ___ __  ___ ___  
+// |  \| |/__\ /' _/ / _//__\| _ \ __| 
+// | | ' | \/ |`._`.| \_| \/ | v / _|  
+// |_|\__|\__/ |___/ \__/\__/|_|_\___| 
+// 
+// Copyright (C) 2018 - NosCore
+// 
+// NosCore is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace NosCore.FriendServer
+{
+    public static class FriendServerBootstrap
+    {
+        public static void Main()
+        {
+            var host = BuildWebHost(null);
+            // ReSharper disable PossibleNullReferenceException
+            (host.ServerFeatures[typeof(IServerAddressesFeature)] as IServerAddressesFeature).Addresses.Add(
+                (host.Services.GetService(typeof(IServerAddressesFeature)) as IServerAddressesFeature)?.Addresses
+                .First());
+            // ReSharper enable PossibleNullReferenceException
+            host.Run();
+        }
+
+        private static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddSerilog();
+                })
+                .UseStartup<Startup>()
+                .PreferHostingUrls(true)
+                .SuppressStatusMessages(true)
+                .Build();
+        }
+    }
+}
