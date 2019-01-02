e1a9ec546f0b7066f64dea45842630d6c7258977
Fizo55 <hugomacees56000@gmail.com>
2019-01-02 23:49:43 +0100

    Update StatController.cs

--- src/NosCore.WorldServer/Controllers/StatController.cs
+++ src/NosCore.WorldServer/Controllers/StatController.cs
@@ -17,7 +17,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System;
 using Microsoft.AspNetCore.Mvc;
 using NosCore.Configuration;
 using NosCore.Core;
