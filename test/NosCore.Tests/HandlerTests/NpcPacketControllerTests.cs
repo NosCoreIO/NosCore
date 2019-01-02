4b85e5fb2a8e34f1669284620593eb9d3083464a
0Lucifer0 <erwan.joly@gmail.com>
2019-01-02 21:25:38 +1300

    add too rich and tests

--- test/NosCore.Tests/HandlerTests/NpcPacketControllerTests.cs
+++ test/NosCore.Tests/HandlerTests/NpcPacketControllerTests.cs
@@ -394,7 +394,7 @@ namespace NosCore.Tests.HandlerTests
             var itemBuilder = new ItemBuilderService(items, new List<IHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
 
             var list = new ConcurrentDictionary<int, ShopItem>();
-            list.TryAdd(0, new ShopItem { Slot = 0, ItemInstance = itemBuilder.Create(1, -1), Type = 0, Amount = 98});
+            list.TryAdd(0, new ShopItem { Slot = 0, ItemInstance = itemBuilder.Create(1, -1), Type = 0, Amount = 98 });
             var shop = new Shop
             {
                 ShopItems = list
@@ -414,13 +414,13 @@ namespace NosCore.Tests.HandlerTests
             var itemBuilder = new ItemBuilderService(items, new List<IHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
 
             var list = new ConcurrentDictionary<int, ShopItem>();
-            list.TryAdd(0, new ShopItem { Slot = 0, ItemInstance = itemBuilder.Create(1, -1), Type = 0});
+            list.TryAdd(0, new ShopItem { Slot = 0, ItemInstance = itemBuilder.Create(1, -1), Type = 0 });
             var shop = new Shop
             {
                 ShopItems = list
             };
             _session.Character.Buy(shop, 0, 99);
-            
+
             var packet = (SMemoPacket)_session.LastPacket;
             Assert.IsTrue(packet.Message == Language.Instance.GetMessageFromKey(LanguageKey.NOT_ENOUGH_MONEY, _session.Account.Language));
         }
@@ -451,7 +451,7 @@ namespace NosCore.Tests.HandlerTests
         public void UserCantShopWithoutPlace()
         {
             _session.Character.Gold = 500000;
-          
+
             var items = new List<Item>
             {
                 new Item {Type = PocketType.Etc, VNum = 1, IsSoldable = true, Price = 1},
@@ -495,7 +495,7 @@ namespace NosCore.Tests.HandlerTests
             _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 3, 1), PocketType.Etc, 2);
 
             _session.Character.Buy(shop, 0, 998);
-            Assert.IsTrue(_session.Character.Inventory.All(s=>s.Value.Amount == 999));
+            Assert.IsTrue(_session.Character.Inventory.All(s => s.Value.Amount == 999));
             Assert.IsTrue(_session.Character.Gold == 499002);
 
         }
@@ -527,6 +527,103 @@ namespace NosCore.Tests.HandlerTests
 
         }
 
-        //test buyfrom
+        private ClientSession prepareSessionShop()
+        {
+            var conf = new WorldConfiguration() { BackpackSize = 3, MaxItemAmount = 999, MaxGoldAmount = 999_999_999 };
+            var session2 = new ClientSession(conf, new List<PacketController> { new DefaultPacketController(null, _instanceAccessService, null) }, _instanceAccessService);
+            var channelMock = new Mock<IChannel>();
+            session2.RegisterChannel(channelMock.Object);
+            var account = new AccountDto { Name = "AccountTest", Password = "test".ToSha512() };
+            session2.InitializeAccount(account);
+            session2.SessionId = 1;
+          
+            _handler = new NpcPacketController(conf,
+                new NrunAccessService(new List<IHandler<Tuple<IAliveEntity, NrunPacket>, Tuple<IAliveEntity, NrunPacket>>>()));
+            _handler.RegisterSession(session2);
+            session2.SetCharacter(new CharacterDto
+            {
+                CharacterId = 1,
+                Name = "chara2",
+                Slot = 1,
+                AccountId = 1,
+                MapId = 1,
+                State = CharacterState.Active
+            }.Adapt<Character>());
+            var mapinstance = _instanceAccessService.GetBaseMapById(0);
+            session2.Character.Account = account;
+            session2.Character.MapInstance = _instanceAccessService.GetBaseMapById(0);
+            session2.Character.MapInstance = mapinstance;
+            session2.Character.Inventory = new InventoryService(new List<Item>(), conf);
+
+            _session.Character.Gold = 500000;
+            var items = new List<Item>
+            {
+                new Item {Type = PocketType.Etc, VNum = 1, IsSoldable = true, Price = 1},
+            };
+            var itemBuilder = new ItemBuilderService(items, new List<IHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
+            _session.Character.ItemBuilderService = itemBuilder;
+            var list = new ConcurrentDictionary<int, ShopItem>();
+            var it = itemBuilder.Create(1, 1, 999);
+            session2.Character.Inventory.AddItemToPocket(it, PocketType.Etc, 0);
+            list.TryAdd(0, new ShopItem { Slot = 0, ItemInstance = it, Type = 0, Price = 1, Amount = 999 });
+            list.TryAdd(1, new ShopItem { Slot = 1, ItemInstance = it, Type = 0, Price = 1, Amount = 500 });
+            session2.Character.Shop = new Shop
+            {
+                Session = session2,
+                ShopItems = list
+            };
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 1, 999), PocketType.Etc, 0);
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 2, 999), PocketType.Etc, 1);
+            return session2;
+        }
+
+        [TestMethod]
+        public void UserCanShopFromSession()
+        {
+            var session2 = prepareSessionShop();
+            _session.Character.Buy(session2.Character.Shop, 0, 999);
+            Assert.IsTrue(session2.Character.Gold == 999);
+            Assert.IsTrue(session2.Character.Inventory.CountItem(1) == 0);
+        }
+
+        [TestMethod]
+        public void UserCanShopFromSessionPartial()
+        {
+            var session2 = prepareSessionShop();
+            _session.Character.Buy(session2.Character.Shop, 0, 998);
+            Assert.IsTrue(session2.Character.Gold == 998);
+            Assert.IsTrue(session2.Character.Inventory.CountItem(1) == 1);
+        }
+
+        [TestMethod]
+        public void UserCanNotShopMoreThanShop()
+        {
+            var session2 = prepareSessionShop();
+            _session.Character.Buy(session2.Character.Shop, 1, 501);
+            Assert.IsTrue(session2.Character.Gold == 0);
+            Assert.IsTrue(session2.Character.Inventory.CountItem(1) == 999);
+        }
+
+        [TestMethod]
+        public void UserCanShopFull()
+        {
+            var session2 = prepareSessionShop();
+            _session.Character.Buy(session2.Character.Shop, 1, 500);
+            Assert.IsTrue(session2.Character.Gold == 500);
+            Assert.IsTrue(session2.Character.Inventory.CountItem(1) == 499);
+        }
+
+        [TestMethod]
+        public void UserCanNotShopTooRich()
+        {
+            var session2 = prepareSessionShop();
+            session2.Character.Gold = 999_999_999;
+            _session.Character.Buy(session2.Character.Shop, 0, 999);
+            Assert.IsTrue(session2.Character.Gold == 999_999_999);
+            Assert.IsTrue(session2.Character.Inventory.CountItem(1) == 999);
+
+            var packet = (SMemoPacket)_session.LastPacket;
+            Assert.IsTrue(packet.Message == Language.Instance.GetMessageFromKey(LanguageKey.TOO_RICH_SELLER, _session.Account.Language));
+        }
     }
 }
