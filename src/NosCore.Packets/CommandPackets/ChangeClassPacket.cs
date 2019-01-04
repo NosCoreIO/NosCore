158eace6d31f54148bdd1226d4ee67ae9dc8951d
Hugo <hugomacees56000@gmail.com>
2019-01-04 12:42:03 +0100

    Add changeclass command

--- src/NosCore.Packets/CommandPackets/ChangeClassPacket.cs
+++ src/NosCore.Packets/CommandPackets/ChangeClassPacket.cs
@@ -17,15 +17,25 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace NosCore.Shared.Enumerations
+using System.ComponentModel.DataAnnotations;
+using NosCore.Core.Serializing;
+using NosCore.Shared.Enumerations.Account;
+
+namespace NosCore.Packets.CommandPackets
 {
-    public enum UpdateStatActionType : byte
+    [PacketHeader("$ChangeClass", Authority = AuthorityType.GameMaster)]
+    public class ChangeClassPacket : PacketDefinition, ICommandPacket
     {
-        UpdateLevel,
-        UpdateJobLevel,
-        UpdateHeroLevel,
-        UpdateReputation,
-        UpdateGold,
-        UpdateClass
+        [PacketIndex(0)]
+        [Range(0, 4)]
+        public byte ClassType { get; set; }
+
+        [PacketIndex(1, SerializeToEnd = true)]
+        public string Name { get; set; }
+
+        public string Help()
+        {
+            return "$ChangeClass VALUE NAME";
+        }
     }
 }
