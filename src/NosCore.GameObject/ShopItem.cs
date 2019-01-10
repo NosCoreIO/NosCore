3cfb376299d1bb4bf6c0996dbcb4cb0e918d20cb
Elendan <arthur.despeaux@hotmail.fr>
2019-01-10 21:51:45 +0100

    Fix tests

--- src/NosCore.GameObject/ShopItem.cs
+++ src/NosCore.GameObject/ShopItem.cs
@@ -27,7 +27,7 @@ namespace NosCore.GameObject
     {
         public IItemInstance ItemInstance { get; set; }
 
-        public byte Type { get; set; }
+        public short Type { get; set; }
 
         public short Slot { get; set; }
 
