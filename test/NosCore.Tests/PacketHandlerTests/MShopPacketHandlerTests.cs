68352500fda186c02dfd0d889e78cf32102a2520
0Lucifer0 <erwan.joly@gmail.com>
2019-04-21 21:04:57 +1200

    fix tests

---
+++ test/NosCore.Tests/PacketHandlerTests/MShopPacketHandlerTests.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ChickenAPI.Packets.ClientPackets.Drops;
+using ChickenAPI.Packets.ClientPackets.Inventory;
+using ChickenAPI.Packets.ClientPackets.Login;
+using ChickenAPI.Packets.ClientPackets.Movement;
+using ChickenAPI.Packets.ClientPackets.Shops;
+using ChickenAPI.Packets.Enumerations;
+using ChickenAPI.Packets.ServerPackets.Chats;
+using ChickenAPI.Packets.ServerPackets.Login;
+using ChickenAPI.Packets.ServerPackets.UI;
+using DotNetty.Transport.Channels;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NosCore.Configuration;
+using NosCore.Core;
+using NosCore.Core.Encryption;
+using NosCore.Core.I18N;
+using NosCore.Core.Networking;
+using NosCore.Data;
+using NosCore.Data.AliveEntities;
+using NosCore.Data.Enumerations;
+using NosCore.Data.Enumerations.Character;
+using NosCore.Data.Enumerations.Group;
+using NosCore.Data.Enumerations.I18N;
+using NosCore.Data.StaticEntities;
+using NosCore.Data.WebApi;
+using NosCore.Database;
+using NosCore.Database.DAL;
+using NosCore.GameObject;
+using NosCore.GameObject.Map;
+using NosCore.GameObject.Networking;
+using NosCore.GameObject.Networking.ClientSession;
+using NosCore.GameObject.Providers.InventoryService;
+using NosCore.GameObject.Providers.ItemProvider;
+using NosCore.GameObject.Providers.ItemProvider.Item;
+using NosCore.GameObject.Providers.MapInstanceProvider;
+using NosCore.GameObject.Providers.MapItemProvider;
+using NosCore.PacketHandlers.Login;
+using NosCore.PacketHandlers.Shops;
+using Serilog;
+using Character = NosCore.GameObject.Character;
+
+namespace NosCore.Tests.PacketHandlerTests
+{
+    [TestClass]
+    public class MShopPacketHandlerTests
+    {
+        private static readonly ILogger _logger = Logger.GetLoggerConfiguration().CreateLogger();
+        private readonly IGenericDao<MapDto> _mapDao = new GenericDao<Database.Entities.Map, MapDto>(_logger);
+        private readonly IGenericDao<AccountDto> _accountDao = new GenericDao<Database.Entities.Account, AccountDto>(_logger);
+        private readonly IGenericDao<CharacterDto> _characterDao = new GenericDao<Database.Entities.Character, CharacterDto>(_logger);
+        private readonly IGenericDao<CharacterRelationDto> _characterRelationDao = new GenericDao<Database.Entities.CharacterRelation, CharacterRelationDto>(_logger);
+        private readonly IGenericDao<PortalDto> _portalDao = new GenericDao<Database.Entities.Portal, PortalDto>(_logger);
+        private readonly IGenericDao<MapMonsterDto> _mapMonsterDao = new GenericDao<Database.Entities.MapMonster, MapMonsterDto>(_logger);
+        private readonly IGenericDao<MapNpcDto> _mapNpcDao = new GenericDao<Database.Entities.MapNpc, MapNpcDto>(_logger);
+        private readonly IGenericDao<IItemInstanceDto> _itemInstanceDao = new ItemInstanceDao(_logger);
+        private readonly Map _map = new Map
+        {
+            MapId = 0,
+            Name = "testMap",
+            Data = new byte[]
+            {
+                8, 0, 8, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 1, 1, 1, 0, 0, 0, 0,
+                0, 1, 1, 1, 0, 0, 0, 0,
+                0, 1, 1, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            }
+        };
+
+        private readonly Map _mapShop = new Map
+        {
+            MapId = 1,
+            Name = "shopMap",
+            ShopAllowed = true,
+            Data = new byte[]
+            {
+                8, 0, 8, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 1, 1, 1, 0, 0, 0, 0,
+                0, 1, 1, 1, 0, 0, 0, 0,
+                0, 1, 1, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            }
+        };
+
+        private readonly MShopPacket shopPacket = new MShopPacket
+        {
+            Type = CreateShopPacketType.Open,
+            ItemList = new List<MShopItemSubPacket>
+            {
+                new MShopItemSubPacket {Type = PocketType.Etc, Slot = 0, Amount = 1, Price = 10000},
+                new MShopItemSubPacket {Type = PocketType.Etc, Slot = 1, Amount = 2, Price = 20000},
+                new MShopItemSubPacket {Type = PocketType.Etc, Slot = 2, Amount = 3, Price = 30000},
+            },
+            Name = "TEST SHOP"
+        };
+
+
+        MapInstanceProvider _instanceProvider;
+        private ClientSession _session;
+        private MShopPacketHandler _mShopPacketHandler;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            TypeAdapterConfig.GlobalSettings.ForDestinationType<IInitializable>().AfterMapping(dest => Task.Run(() => dest.Initialize()));
+            Broadcaster.Reset();
+            var contextBuilder =
+                new DbContextOptionsBuilder<NosCoreContext>().UseInMemoryDatabase(
+                    databaseName: Guid.NewGuid().ToString());
+            DataAccessHelper.Instance.InitializeForTest(contextBuilder.Options);
+            var map = new MapDto { MapId = 1 };
+            _mapDao.InsertOrUpdate(ref map);
+            var account = new AccountDto { Name = "AccountTest", Password = "test".ToSha512() };
+            _accountDao.InsertOrUpdate(ref account);
+            WebApiAccess.RegisterBaseAdress();
+            WebApiAccess.Instance.MockValues =
+                new Dictionary<WebApiRoute, object>
+                {
+                    {WebApiRoute.Channel, new List<ChannelInfo> {new ChannelInfo()}},
+                    {WebApiRoute.ConnectedAccount, new List<ConnectedAccount>()}
+                };
+
+            var conf = new WorldConfiguration { BackpackSize = 3, MaxItemAmount = 999, MaxGoldAmount = 999_999_999 };
+            var _chara = new Character(new InventoryService(new List<ItemDto>(), conf, _logger), null, null, _characterRelationDao, _characterDao, _itemInstanceDao, _accountDao, _logger, null)
+            {
+                CharacterId = 1,
+                Name = "TestExistingCharacter",
+                Slot = 1,
+                AccountId = account.AccountId,
+                MapId = 1,
+                State = CharacterState.Active
+            };
+
+            ItemProvider itemProvider = new ItemProvider(new List<ItemDto>(),
+                new List<IEventHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
+            _instanceProvider = new MapInstanceProvider(new List<MapDto> { _map, _mapShop },
+                new MapItemProvider(new List<IEventHandler<MapItem, Tuple<MapItem, GetPacket>>>()),
+                _mapNpcDao,
+                _mapMonsterDao, _portalDao, new Adapter(), _logger);
+            _instanceProvider.Initialize();
+            var channelMock = new Mock<IChannel>();
+            _session = new ClientSession(null, _logger, null);
+            _session.RegisterChannel(channelMock.Object);
+            _session.InitializeAccount(account);
+            _session.SessionId = 1;
+            _session.SetCharacter(_chara);
+            var mapinstance = _instanceProvider.GetBaseMapById(0);
+            _session.Character.Account = account;
+            _session.Character.MapInstance = mapinstance;
+            _session.Character.MapInstance.Portals = new List<Portal>
+            {
+                new Portal
+                {
+                    DestinationMapId = _map.MapId,
+                    Type = PortalType.Open,
+                    SourceMapInstanceId = mapinstance.MapInstanceId,
+                    DestinationMapInstanceId = mapinstance.MapInstanceId,
+                    DestinationX = 5,
+                    DestinationY = 5,
+                    PortalId = 1,
+                    SourceMapId = _map.MapId,
+                    SourceX = 0,
+                    SourceY = 0,
+                }
+            };
+
+            Broadcaster.Instance.RegisterSession(_session);
+            _mShopPacketHandler = new MShopPacketHandler();
+        }
+
+        [TestMethod]
+        public void UserCanNotCreateShopCloseToPortal()
+        {
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            var packet = (MsgPacket)_session.LastPacket;
+            Assert.IsTrue(packet.Message ==
+                Language.Instance.GetMessageFromKey(LanguageKey.SHOP_NEAR_PORTAL, _session.Account.Language));
+            Assert.IsNull(_session.Character.Shop);
+        }
+
+        [TestMethod]
+        public void UserCanNotCreateShopInTeam()
+        {
+            _session.Character.PositionX = 7;
+            _session.Character.PositionY = 7;
+            _session.Character.Group = new Group(GroupType.Team);
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            var packet = (MsgPacket)_session.LastPacket;
+            Assert.IsTrue(packet.Message ==
+                Language.Instance.GetMessageFromKey(LanguageKey.SHOP_NOT_ALLOWED_IN_RAID, _session.Account.Language));
+            Assert.IsNull(_session.Character.Shop);
+        }
+
+        [TestMethod]
+        public void UserCanCreateShopInGroup()
+        {
+            _session.Character.PositionX = 7;
+            _session.Character.PositionY = 7;
+            _session.Character.Group = new Group(GroupType.Group);
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            var packet = (MsgPacket)_session.LastPacket;
+            Assert.IsTrue(packet.Message !=
+                Language.Instance.GetMessageFromKey(LanguageKey.SHOP_NOT_ALLOWED_IN_RAID, _session.Account.Language));
+        }
+
+        [TestMethod]
+        public void UserCanNotCreateShopInNotShopAllowedMaps()
+        {
+            _session.Character.PositionX = 7;
+            _session.Character.PositionY = 7;
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            var packet = (MsgPacket)_session.LastPacket;
+            Assert.IsTrue(packet.Message ==
+                Language.Instance.GetMessageFromKey(LanguageKey.SHOP_NOT_ALLOWED, _session.Account.Language));
+            Assert.IsNull(_session.Character.Shop);
+        }
+
+
+        [TestMethod]
+        public void UserCanNotCreateShopWithMissingItem()
+        {
+            var items = new List<ItemDto>
+            {
+                new Item {Type = PocketType.Etc, VNum = 1},
+            };
+            var itemBuilder = new ItemProvider(items, new List<IEventHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
+
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 1));
+            _session.Character.MapInstance = _instanceProvider.GetBaseMapById(1);
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            Assert.IsNull(_session.Character.Shop);
+        }
+
+
+        [TestMethod]
+        public void UserCanNotCreateShopWithMissingAmountItem()
+        {
+            var items = new List<ItemDto>
+            {
+                new Item {Type = PocketType.Etc, VNum = 1},
+            };
+            var itemBuilder = new ItemProvider(items, new List<IEventHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
+
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 1), PocketType.Etc, 0);
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 1), PocketType.Etc, 1);
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 1), PocketType.Etc, 2);
+
+            _session.Character.MapInstance = _instanceProvider.GetBaseMapById(1);
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            Assert.IsNull(_session.Character.Shop);
+            var packet = (SayPacket)_session.LastPacket;
+            Assert.IsTrue(packet.Message ==
+                Language.Instance.GetMessageFromKey(LanguageKey.SHOP_ONLY_TRADABLE_ITEMS, _session.Account.Language));
+        }
+
+        [TestMethod]
+        public void UserCanCreateShop()
+        {
+            var items = new List<ItemDto>
+            {
+                new Item {Type = PocketType.Etc, VNum = 1, IsTradable = true},
+            };
+            var itemBuilder = new ItemProvider(items, new List<IEventHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
+
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 1), PocketType.Etc, 0);
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 2), PocketType.Etc, 1);
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 3), PocketType.Etc, 2);
+
+            _session.Character.MapInstance = _instanceProvider.GetBaseMapById(1);
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            Assert.IsNotNull(_session.Character.Shop);
+        }
+
+        public void UserCanNotCreateShopInExchange()
+        {
+            _session.Character.InExchangeOrTrade = true;
+            var items = new List<ItemDto>
+            {
+                new Item {Type = PocketType.Etc, VNum = 1, IsTradable = true},
+            };
+            var itemBuilder = new ItemProvider(items, new List<IEventHandler<Item, Tuple<IItemInstance, UseItemPacket>>>());
+
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 1), PocketType.Etc, 0);
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 2), PocketType.Etc, 1);
+            _session.Character.Inventory.AddItemToPocket(itemBuilder.Create(1, 3), PocketType.Etc, 2);
+
+            _session.Character.MapInstance = _instanceProvider.GetBaseMapById(1);
+            _mShopPacketHandler.Execute(shopPacket, _session);
+            Assert.IsNull(_session.Character.Shop);
+        }
+
+        [TestMethod]
+        public void UserCanNotCreateEmptyShop()
+        {
+            _session.Character.MapInstance = _instanceProvider.GetBaseMapById(1);
+ 
+            _mShopPacketHandler.Execute(new MShopPacket
+            {
+                Type = CreateShopPacketType.Open,
+                ItemList = new List<MShopItemSubPacket>(),
+                Name = "TEST SHOP"
+            }, _session);
+            Assert.IsNull(_session.Character.Shop);
+            var packet = (SayPacket)_session.LastPacket;
+            Assert.IsTrue(packet.Message ==
+                Language.Instance.GetMessageFromKey(LanguageKey.SHOP_EMPTY, _session.Account.Language));
+        }
+    }
+}
---
+++ test/NosCore.Tests/PacketHandlerTests/UseItemPacketHandlerTests.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChickenAPI.Packets.ClientPackets.Drops;
+using ChickenAPI.Packets.ClientPackets.Inventory;
+using ChickenAPI.Packets.ClientPackets.Login;
+using ChickenAPI.Packets.ClientPackets.Movement;
+using ChickenAPI.Packets.ClientPackets.Shops;
+using ChickenAPI.Packets.Enumerations;
+using ChickenAPI.Packets.ServerPackets.Chats;
+using ChickenAPI.Packets.ServerPackets.Login;
+using ChickenAPI.Packets.ServerPackets.Shop;
+using ChickenAPI.Packets.ServerPackets.UI;
+using DotNetty.Transport.Channels;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NosCore.Configuration;
+using NosCore.Core;
+using NosCore.Core.Encryption;
+using NosCore.Core.I18N;
+using NosCore.Core.Networking;
+using NosCore.Data;
+using NosCore.Data.AliveEntities;
+using NosCore.Data.Enumerations;
+using NosCore.Data.Enumerations.Character;
+using NosCore.Data.Enumerations.I18N;
+using NosCore.Data.Enumerations.Items;
+using NosCore.Data.Enumerations.Map;
+using NosCore.Data.StaticEntities;
+using NosCore.Data.WebApi;
+using NosCore.Database;
+using NosCore.Database.DAL;
+using NosCore.GameObject;
+using NosCore.GameObject.Map;
+using NosCore.GameObject.Networking;
+using NosCore.GameObject.Networking.ClientSession;
+using NosCore.GameObject.Providers.ExchangeProvider;
+using NosCore.GameObject.Providers.InventoryService;
+using NosCore.GameObject.Providers.ItemProvider;
+using NosCore.GameObject.Providers.ItemProvider.Handlers;
+using NosCore.GameObject.Providers.ItemProvider.Item;
+using NosCore.GameObject.Providers.MapInstanceProvider;
+using NosCore.GameObject.Providers.MapItemProvider;
+using NosCore.GameObject.Providers.MapItemProvider.Handlers;
+using NosCore.PacketHandlers.Friend;
+using NosCore.PacketHandlers.Game;
+using NosCore.PacketHandlers.Inventory;
+using NosCore.PacketHandlers.Login;
+using NosCore.PacketHandlers.Shops;
+using Serilog;
+using Character = NosCore.GameObject.Character;
+
+namespace NosCore.Tests.PacketHandlerTests
+{
+    [TestClass]
+    public class UseItemPacketHandlerTests
+    {
+        private UseItemPacketHandler _useItemPacketHandler;
+        private static readonly ILogger _logger = Logger.GetLoggerConfiguration().CreateLogger();
+
+        private readonly ClientSession _session = new ClientSession(
+            new WorldConfiguration
+            { BackpackSize = 2, MaxItemAmount = 999, MaxSpPoints = 10_000, MaxAdditionalSpPoints = 1_000_000 }, _logger, new List<IPacketHandler>());
+
+        private Character _chara;
+        private IItemProvider _item;
+        private MapInstance _map;
+        private MapItemProvider _mapItemProvider;
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            SystemTime.Freeze(DateTime.Now);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            SystemTime.Freeze();
+            var contextBuilder =
+                new DbContextOptionsBuilder<NosCoreContext>().UseInMemoryDatabase(
+                    databaseName: Guid.NewGuid().ToString());
+            DataAccessHelper.Instance.InitializeForTest(contextBuilder.Options);
+            var _acc = new AccountDto { Name = "AccountTest", Password = "test".ToSha512() };
+
+            var items = new List<ItemDto>
+            {
+                new Item {Type = PocketType.Main, VNum = 1012, IsDroppable = true},
+                new Item {Type = PocketType.Main, VNum = 1013},
+                new Item {Type = PocketType.Equipment, VNum = 1, ItemType = ItemType.Weapon},
+                new Item {Type = PocketType.Equipment, VNum = 2, EquipmentSlot = EquipmentType.Fairy, Element = 2},
+                new Item
+                {
+                    Type = PocketType.Equipment, VNum = 912, ItemType = ItemType.Specialist, ReputationMinimum = 2,
+                    Element = 1
+                },
+                new Item {Type = PocketType.Equipment, VNum = 924, ItemType = ItemType.Fashion},
+                new Item
+                {
+                    Type = PocketType.Main, VNum = 1078, ItemType = ItemType.Special,
+                    Effect = ItemEffectType.DroppedSpRecharger, EffectValue = 10_000, WaitDelay = 5_000
+                }
+            };
+
+            _chara = new Character(new InventoryService(items, _session.WorldConfiguration, _logger),
+                new ExchangeProvider(null, null, _logger), null, null, null, null, null, _logger, null)
+            {
+                CharacterId = 1,
+                Name = "TestExistingCharacter",
+                Slot = 1,
+                AccountId = _acc.AccountId,
+                MapId = 1,
+                State = CharacterState.Active
+            };
+            _session.InitializeAccount(_acc);
+
+            _item = new ItemProvider(items, new List<IEventHandler<Item, Tuple<IItemInstance, UseItemPacket>>>
+            {
+                new SpRechargerEventHandler(_session.WorldConfiguration),
+                new VehicleEventHandler(_logger),
+                new WearEventHandler(_logger)
+            });
+
+            _mapItemProvider = new MapItemProvider(new List<IEventHandler<MapItem, Tuple<MapItem, GetPacket>>>
+                {new DropEventHandler(), new SpChargerEventHandler(), new GoldDropEventHandler()});
+            _map = new MapInstance(new Map
+            {
+                Name = "testMap",
+                Data = new byte[]
+                    {
+                        8, 0, 8, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 1, 1, 1, 0, 0, 0, 0,
+                        0, 1, 1, 1, 0, 0, 0, 0,
+                        0, 1, 1, 1, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0
+                    }
+            }
+                , Guid.NewGuid(), false, MapInstanceType.BaseMapInstance,
+                _mapItemProvider,
+                null, _logger);
+            _session.SetCharacter(_chara);
+            _session.Character.MapInstance = _map;
+            _session.Character.Account = _acc;
+            _useItemPacketHandler = new UseItemPacketHandler();
+        }
+
+        [TestMethod]
+        public void Test_Binding()
+        {
+            var items = new List<ItemDto>
+            {
+                new Item {Type = PocketType.Equipment, VNum = 1, RequireBinding = true},
+            };
+            _item = new ItemProvider(items,
+                new List<IEventHandler<Item, Tuple<IItemInstance, UseItemPacket>>> { new WearEventHandler(_logger) });
+
+            _session.Character.Inventory.AddItemToPocket(_item.Create(1, 1));
+            _useItemPacketHandler.Execute(new UseItemPacket { Slot = 0, Type = PocketType.Equipment, Mode = 1 }, _session);
+
+            Assert.IsTrue(_session.Character.Inventory.Any(s =>
+                s.Value.ItemVNum == 1 && s.Value.Type == PocketType.Wear &&
+                s.Value.BoundCharacterId == _session.Character.VisualId));
+        }
+
+        [TestMethod]
+        public void Test_Increment_SpAdditionPoints()
+        {
+            _session.Character.SpAdditionPoint = 0;
+            _session.Character.Inventory.AddItemToPocket(_item.Create(1078, 1));
+            var item = _session.Character.Inventory.First();
+            _useItemPacketHandler.Execute(new UseItemPacket
+            {
+                VisualType = VisualType.Player,
+                VisualId = 1,
+                Type = item.Value.Type,
+                Slot = item.Value.Slot,
+                Mode = 0,
+                Parameter = 0
+            }, _session);
+            Assert.IsTrue(_session.Character.SpAdditionPoint != 0 && !(_session.LastPacket is MsgPacket));
+        }
+
+        [TestMethod]
+        public void Test_Overflow_SpAdditionPoints()
+        {
+            _session.Character.SpAdditionPoint = _session.WorldConfiguration.MaxAdditionalSpPoints;
+            _session.Character.Inventory.AddItemToPocket(_item.Create(1078, 1));
+            var item = _session.Character.Inventory.First();
+            _useItemPacketHandler.Execute(new UseItemPacket
+            {
+                VisualType = VisualType.Player,
+                VisualId = 1,
+                Type = item.Value.Type,
+                Slot = item.Value.Slot,
+                Mode = 0,
+                Parameter = 0
+            }, _session);
+            var packet = (MsgPacket)_session.LastPacket;
+            Assert.IsTrue(_session.Character.SpAdditionPoint == _session.WorldConfiguration.MaxAdditionalSpPoints &&
+                packet.Message == Language.Instance.GetMessageFromKey(LanguageKey.SP_ADDPOINTS_FULL,
+                    _session.Character.Account.Language));
+        }
+
+        [TestMethod]
+        public void Test_CloseToLimit_SpAdditionPoints()
+        {
+            _session.Character.SpAdditionPoint = _session.WorldConfiguration.MaxAdditionalSpPoints - 1;
+            _session.Character.Inventory.AddItemToPocket(_item.Create(1078, 1));
+            var item = _session.Character.Inventory.First();
+            _useItemPacketHandler.Execute(new UseItemPacket
+            {
+                VisualType = VisualType.Player,
+                VisualId = 1,
+                Type = item.Value.Type,
+                Slot = item.Value.Slot,
+                Mode = 0,
+                Parameter = 0
+            }, _session);
+            Assert.IsTrue(_session.Character.SpAdditionPoint == _session.WorldConfiguration.MaxAdditionalSpPoints &&
+                !(_session.LastPacket is MsgPacket));
+        }
+
+    }
+}
