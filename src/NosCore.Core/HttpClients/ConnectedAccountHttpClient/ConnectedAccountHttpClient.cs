31a9fd217ec8d64c27323a62869f2dba487ed586
0Lucifer0 <erwan.joly@gmail.com>
2019-07-14 17:49:28 +1200

    start implementation of httpclients

--- src/NosCore.Core/HttpClients/ConnectedAccountHttpClient/ConnectedAccountHttpClient.cs
+++ src/NosCore.Core/HttpClients/ConnectedAccountHttpClient/ConnectedAccountHttpClient.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using NosCore.Configuration;
+using NosCore.Core.HttpClients.ChannelHttpClient;
 using NosCore.Data.WebApi;
 using NosCore.GameObject.HttpClients.ConnectedAccountHttpClient;
 
@@ -8,13 +11,29 @@ namespace NosCore.Core.HttpClients.ConnectedAccountHttpClient
 {
     public class ConnectedAccountHttpClient : IConnectedAccountHttpClient
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly Channel _channel;
+        private readonly IChannelHttpClient _channelHttpClient;
+
+        public ConnectedAccountHttpClient(IHttpClientFactory httpClientFactory, Channel channel, IChannelHttpClient channelHttpClient)
+        {
+            _httpClientFactory = httpClientFactory;
+            _channel = channel;
+            _channelHttpClient = channelHttpClient;
+        }
+
         public void Disconnect(ServerConfiguration receiverItem1, long connectedCharacterId)
         {
-            throw new NotImplementedException();
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_channel.MasterCommunication.ToString());
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _channelHttpClient.GetOrRefreshToken());
         }
 
         public (ServerConfiguration, ConnectedAccount) GetCharacter(long? characterId, string characterName)
         {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_channel.MasterCommunication.ToString());
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _channelHttpClient.GetOrRefreshToken());
             //var channels = MasterClientListSingleton.Instance.Channels ?? Get<List<ChannelInfo>>(WebApiRoute.Channel);
             //foreach (var channel in (channels ?? new List<ChannelInfo>()).Where(c => c.Type == ServerType.WorldServer))
             //{
@@ -33,7 +52,9 @@ namespace NosCore.Core.HttpClients.ConnectedAccountHttpClient
 
         public List<ConnectedAccount> GetConnectedAccount(ServerConfiguration serverWebApi)
         {
-            throw new NotImplementedException();
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(serverWebApi.ToString());
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _channelHttpClient.GetOrRefreshToken());
         }
     }
 }
