14436ee3964bd3bb404957d2032e87500dfe8b3e
0Lucifer0 <erwan.joly@gmail.com>
2019-07-26 20:54:54 +1200

    add first tests and fix bazaar dupe

--- test/NosCore.Tests/BazaarTests/BazaarTests.cs
+++ test/NosCore.Tests/BazaarTests/BazaarTests.cs
@@ -3,47 +3,96 @@ using Moq;
 using NosCore.Core;
 using NosCore.Data;
 using NosCore.Data.AliveEntities;
+using NosCore.Data.Enumerations.I18N;
 using NosCore.MasterServer.Controllers;
 using NosCore.MasterServer.DataHolders;
 using System;
+using System.Linq.Expressions;
 
 namespace NosCore.Tests.BazaarTests
 {
     [TestClass]
     public class BazaarTests
     {
-        private BazaarController _bazaarController { get; set; }
+        private BazaarItemsHolder _bazaarItemsHolder;
+        private BazaarController _bazaarController;
+        private Guid _guid;
+        private Mock<IGenericDao<IItemInstanceDto>> _mockItemDao;
 
         [TestInitialize]
         public void Setup()
         {
+            _guid = Guid.NewGuid();
             var mockBzDao = new Mock<IGenericDao<BazaarItemDto>>();
-            var mockItemDao = new Mock<IGenericDao<IItemInstanceDto>>();
+            _mockItemDao = new Mock<IGenericDao<IItemInstanceDto>>();
             var mockCharacterDao = new Mock<IGenericDao<CharacterDto>>();
             var itemList = new System.Collections.Generic.List<ItemDto>();
-            _bazaarController = new BazaarController(new BazaarItemsHolder(mockBzDao.Object, mockItemDao.Object, itemList, mockCharacterDao.Object), mockBzDao.Object, mockItemDao.Object);
-        }
-
-        [TestMethod]
-        public void AddToBazaar()
-        {
-            throw new NotImplementedException();
-        }
-
-        [TestMethod]
-        public void AddToBazaarLimitExceeded()
-        {
-            throw new NotImplementedException();
+            _bazaarItemsHolder = new BazaarItemsHolder(mockBzDao.Object, _mockItemDao.Object, itemList, mockCharacterDao.Object);
+            _bazaarController = new BazaarController(_bazaarItemsHolder, mockBzDao.Object, _mockItemDao.Object);
         }
 
         [TestMethod]
         public void AddToBazaarAllStack()
         {
-            throw new NotImplementedException();
+            _mockItemDao
+                      .Setup(s => s.FirstOrDefault(It.IsAny<Expression<Func<IItemInstanceDto, bool>>>()))
+                      .Returns(new ItemInstanceDto
+                      {
+                          Id = _guid,
+                          Amount = 99
+                      });
+            var add = _bazaarController.AddBazaar(
+                new Data.WebApi.BazaarRequest
+                {
+                    Amount = 99,
+                    CharacterId = 1,
+                    CharacterName = "test",
+                    Duration = 3600,
+                    HasMedal = false,
+                    IsPackage = false,
+                    ItemInstanceId = _guid,
+                    Price = 50
+                });
+            Assert.AreEqual(_bazaarItemsHolder.BazaarItems[0].BazaarItem.ItemInstanceId, _guid);
+            Assert.AreEqual(_bazaarItemsHolder.BazaarItems[0].BazaarItem.Amount, 99);
+            Assert.AreEqual(LanguageKey.OBJECT_IN_BAZAAR, add);
         }
 
         [TestMethod]
         public void AddToBazaarPartialStack()
+        {
+            _mockItemDao
+                      .Setup(s => s.FirstOrDefault(It.IsAny<Expression<Func<IItemInstanceDto, bool>>>()))
+                      .Returns(new ItemInstanceDto
+                      {
+                          Id = _guid,
+                          Amount = 99
+                      });
+            var add = _bazaarController.AddBazaar(
+                new Data.WebApi.BazaarRequest
+                {
+                    Amount = 50,
+                    CharacterId = 1,
+                    CharacterName = "test",
+                    Duration = 3600,
+                    HasMedal = false,
+                    IsPackage = false,
+                    ItemInstanceId = _guid,
+                    Price = 50
+                });
+            Assert.AreNotEqual(_bazaarItemsHolder.BazaarItems[0].BazaarItem.ItemInstanceId, _guid);
+            Assert.AreEqual(_bazaarItemsHolder.BazaarItems[0].BazaarItem.Amount, 50);
+            Assert.AreEqual(LanguageKey.OBJECT_IN_BAZAAR, add);
+        }
+
+        [TestMethod]
+        public void AddToBazaarNegativeAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void AddToBazaarMoreThanItem()
         {
             throw new NotImplementedException();
         }
