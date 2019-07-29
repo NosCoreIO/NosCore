3ec3da249daa1addcd3802a3c37ea5e180171243
0Lucifer0 <erwan.joly@gmail.com>
2019-07-29 19:38:42 +1200

    more tests

--- test/NosCore.Tests/BazaarTests/CScalcPacketHandlerTests.cs
+++ test/NosCore.Tests/BazaarTests/CScalcPacketHandlerTests.cs
@@ -7,6 +7,7 @@ using Moq;
 using NosCore.GameObject.Networking.ClientSession;
 using NosCore.GameObject.HttpClients.BazaarHttpClient;
 using NosCore.GameObject.Providers.ItemProvider;
+using System;
 
 namespace NosCore.Tests.BazaarTests
 {
@@ -24,6 +25,67 @@ namespace NosCore.Tests.BazaarTests
         {
             var conf = new WorldConfiguration();
             _cScalcPacketHandler = new CScalcPacketHandler(conf, _bazaarHttpClient.Object, _itemProvider.Object, _logger);
+        }
+
+        [TestMethod]
+        public void RetrieveWhenInExchangeOrTrade()
+        {
+            //clientSession.SendPacket(new RCScalcPacket { Type = VisualType.Player, Price = 0, RemainingAmount = 0, Amount = 0, Taxes = 0, Total = 0 });
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void RetrieveWhenNoItem()
+        {
+            //clientSession.SendPacket(new RCScalcPacket { Type = VisualType.Player, Price = 0, RemainingAmount = 0, Amount = 0, Taxes = 0, Total = 0 });
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void RetrieveWhenNotYourItem()
+        {
+            //clientSession.SendPacket(new RCScalcPacket { Type = VisualType.Player, Price = 0, RemainingAmount = 0, Amount = 0, Taxes = 0, Total = 0 });
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void RetrieveWhenNotEnoughPlace()
+        {
+            //                    clientSession.SendPacket(new InfoPacket
+            //                    {
+            //                        Message = Language.Instance.GetMessageFromKey(LanguageKey.NOT_ENOUGH_PLACE,
+            //                            clientSession.Account.Language)
+            //                    });
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void RetrieveWhenMaxGold()
+        {
+            //                        clientSession.SendPacket(new MsgPacket
+            //                        {
+            //                            Message = Language.Instance.GetMessageFromKey(LanguageKey.MAX_GOLD,
+            //                                clientSession.Account.Language),
+            //                            Type = MessageType.Whisper
+            //                        });
+            throw new NotImplementedException();
+        }
+
+
+        [TestMethod]
+        public void Retrieve()
+        {
+            //                            clientSession.SendPacket(new RCScalcPacket
+            //                            {
+            //                                Type = VisualType.Player,
+            //                                Price = bz.BazaarItem.Price,
+            //                                RemainingAmount = (short) (bz.BazaarItem.Amount - bz.ItemInstance.Amount),
+            //                                Amount = bz.BazaarItem.Amount,
+            //                                Taxes = taxes,
+            //                                Total = price + taxes
+            //});
+            //                            clientSession.HandlePackets(new[] { new CSListPacket { Index = 0, Filter = BazaarStatusType.Default } });
+            throw new NotImplementedException();
         }
     }
 }
