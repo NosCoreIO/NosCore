a53636eceadef5267f837db8d75a1ae241e09a35
0Lucifer0 <erwan.joly@gmail.com>
2019-08-03 21:14:49 +1200

    finish tests

--- test/NosCore.Tests/BazaarTests/CBListPacketHandlerTests.cs
+++ test/NosCore.Tests/BazaarTests/CBListPacketHandlerTests.cs
@@ -6,6 +6,15 @@ using NosCore.PacketHandlers.CharacterScreen;
 using System.Collections.Generic;
 using NosCore.Data;
 using System;
+using NosCore.Tests.Helpers;
+using NosCore.GameObject.Networking;
+using ChickenAPI.Packets.ClientPackets.Bazaar;
+using System.Linq;
+using ChickenAPI.Packets.ServerPackets.Auction;
+using ChickenAPI.Packets.Enumerations;
+using NosCore.Data.WebApi;
+using NosCore.GameObject.Providers.ItemProvider.Item;
+using NosCore.Core;
 
 namespace NosCore.Tests.BazaarTests
 {
@@ -19,15 +28,90 @@ namespace NosCore.Tests.BazaarTests
         [TestInitialize]
         public void Setup()
         {
-            _cblistPacketHandler = new CBListPacketHandler(_bazaarHttpClient.Object, new List<ItemDto>());
+            TestHelpers.Reset();
+            Broadcaster.Reset();
+            _session = TestHelpers.Instance.GenerateSession();
+            _bazaarHttpClient = new Mock<IBazaarHttpClient>();
+            var items = new List<ItemDto>
+            {
+                new Item {Type = NoscorePocketType.Main, VNum = 1012, IsSoldable = true},
+            };
+            _cblistPacketHandler = new CBListPacketHandler(_bazaarHttpClient.Object, items);
+        }
+
+        [TestMethod]
+        public void ListShouldReturnEmptyWhenNoItems()
+        {
+            _bazaarHttpClient.Setup(b =>
+           b.GetBazaarLinks(
+           It.IsAny<int>(),
+           It.IsAny<int>(),
+           It.IsAny<int>(),
+           It.IsAny<BazaarListType>(),
+           It.IsAny<byte>(),
+           It.IsAny<byte>(),
+           It.IsAny<byte>(),
+           It.IsAny<byte>(),
+           It.IsAny<long?>())
+           ).Returns(new List<BazaarLink>());
+            _cblistPacketHandler.Execute(new CBListPacket { ItemVNumFilter = new List<short>() }, _session);
+            var lastpacket = (RcbListPacket)_session.LastPacket.FirstOrDefault(s => s is RcbListPacket);
+            Assert.IsTrue(lastpacket.Items.Count == 0);
         }
 
         [TestMethod]
         public void ListShouldReturnTheItems()
         {
-            throw new NotImplementedException();
+            _bazaarHttpClient.Setup(b =>
+            b.GetBazaarLinks(
+            It.IsAny<int>(),
+            It.IsAny<int>(),
+            It.IsAny<int>(),
+            It.IsAny<BazaarListType>(),
+            It.IsAny<byte>(),
+            It.IsAny<byte>(),
+            It.IsAny<byte>(),
+            It.IsAny<byte>(),
+            It.IsAny<long?>())
+            ).Returns(new List<BazaarLink> {
+                new BazaarLink
+                {
+                    SellerName = "test",
+                    BazaarItem = new BazaarItemDto { Price = 50, Amount = 1, DateStart = SystemTime.Now(), Duration = 200 },
+                    ItemInstance = new ItemInstanceDto { ItemVNum = 1012, Amount = 1 }
+               }
+            });
+            _cblistPacketHandler.Execute(new CBListPacket { ItemVNumFilter = new List<short>() }, _session);
+            var lastpacket = (RcbListPacket)_session.LastPacket.FirstOrDefault(s => s is RcbListPacket);
+            Assert.IsTrue(lastpacket.Items.Count == 1);
         }
 
+        [TestMethod]
+        public void ListShouldReturnTheItemsNotValid()
+        {
+            _bazaarHttpClient.Setup(b =>
+            b.GetBazaarLinks(
+            It.IsAny<int>(),
+            It.IsAny<int>(),
+            It.IsAny<int>(),
+            It.IsAny<BazaarListType>(),
+            It.IsAny<byte>(),
+            It.IsAny<byte>(),
+            It.IsAny<byte>(),
+            It.IsAny<byte>(),
+            It.IsAny<long?>())
+            ).Returns(new List<BazaarLink> {
+                new BazaarLink
+                {
+                    SellerName = "test",
+                    BazaarItem = new BazaarItemDto { Price = 50, Amount = 1 },
+                    ItemInstance = new ItemInstanceDto { ItemVNum = 1012, Amount = 1 }
+               }
+            });
+            _cblistPacketHandler.Execute(new CBListPacket { ItemVNumFilter = new List<short>() }, _session);
+            var lastpacket = (RcbListPacket)_session.LastPacket.FirstOrDefault(s => s is RcbListPacket);
+            Assert.IsTrue(lastpacket.Items.Count == 0);
+        }
         //todo list filter
     }
 }
--- test/NosCore.Tests/BazaarTests/CBuyPacketHandlerTests.cs
+++ test/NosCore.Tests/BazaarTests/CBuyPacketHandlerTests.cs
@@ -55,6 +55,13 @@ namespace NosCore.Tests.BazaarTests
                     BazaarItem = new BazaarItemDto { Price = 60, Amount = 1 },
                     ItemInstance = new ItemInstanceDto { ItemVNum = 1012 }
                 });
+            _bazaarHttpClient.Setup(b => b.GetBazaarLink(3)).Returns(
+            new BazaarLink
+            {
+                SellerName = "test",
+                BazaarItem = new BazaarItemDto { Price = 50, Amount = 99, IsPackage = true },
+                ItemInstance = new ItemInstanceDto { ItemVNum = 1012, Amount = 99 }
+            });
             _bazaarHttpClient.Setup(b => b.GetBazaarLink(1)).Returns((BazaarLink)null);
             _bazaarHttpClient.Setup(b => b.Remove(It.IsAny<long>(), It.IsAny<int>(), It.IsAny<string>())).Returns(true);
         }
@@ -63,6 +70,13 @@ namespace NosCore.Tests.BazaarTests
         public void BuyWhenExchangeOrTrade()
         {
             _session.Character.InExchangeOrTrade = true;
+            _cbuyPacketHandler.Execute(new CBuyPacket
+            {
+                BazaarId = 1,
+                Price = 50,
+                Amount = 1,
+                VNum = 1012,
+            }, _session);
             Assert.IsNull(_session.LastPacket.FirstOrDefault());
         }
 
@@ -144,9 +158,39 @@ namespace NosCore.Tests.BazaarTests
         }
 
         [TestMethod]
+        public void BuyPartialPackage()
+        {
+            _session.Character.Gold = 5000;
+            _cbuyPacketHandler.Execute(new CBuyPacket
+            {
+                BazaarId = 3,
+                Price = 50,
+                Amount = 1,
+                VNum = 1012,
+            }, _session);
+            Assert.IsNull(_session.LastPacket.FirstOrDefault());
+        }
+
+        [TestMethod]
+        public void BuyPackage()
+        {
+            _session.Character.Gold = 5000;
+            var item = new Item { Type = NoscorePocketType.Main };
+            _itemProvider.Setup(s => s.Convert(It.IsAny<IItemInstanceDto>())).Returns(new ItemInstance { Amount = 1, ItemVNum = 1012, Item = item });
+            _cbuyPacketHandler.Execute(new CBuyPacket
+            {
+                BazaarId = 3,
+                Price = 50,
+                Amount = 99,
+                VNum = 1012,
+            }, _session);
+            var lastpacket = (SayPacket)_session.LastPacket.FirstOrDefault(s => s is SayPacket);
+            Assert.IsTrue(lastpacket.Message == $"{Language.Instance.GetMessageFromKey(LanguageKey.ITEM_ACQUIRED, _session.Account.Language)}: {item.Name[_session.Account.Language]} x {99}");
+        }
+
+        [TestMethod]
         public void BuyNotEnoughMoney()
         {
-
             _cbuyPacketHandler.Execute(new CBuyPacket
             {
                 BazaarId = 0,
