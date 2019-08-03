42f2dec5af9cfba21dd19e100dfbd5cb117a5797
0Lucifer0 <erwan.joly@gmail.com>
2019-08-03 21:45:20 +1200

    start implementing quicklist

---
+++ src/NosCore.Data/QuickListDto.cs
@@ -0,0 +1,45 @@
+﻿//  __  _  __    __   ___ __  ___ ___  
+// |  \| |/__\ /' _/ / _//__\| _ \ __| 
+// | | ' | \/ |`._`.| \_| \/ | v / _|  
+// |_|\__|\__/ |___/ \__/\__/|_|_\___| 
+// 
+// Copyright (C) 2019 - NosCore
+// 
+// NosCore is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NosCore.Data
+{
+    public class QuicklistEntryDto : IDto
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public long CharacterId { get; set; }
+
+        public short Morph { get; set; }
+
+        public short Pos { get; set; }
+
+        public short Q1 { get; set; }
+
+        public short Q2 { get; set; }
+
+        public short Slot { get; set; }
+
+        public short Type { get; set; }
+    }
+
+}
