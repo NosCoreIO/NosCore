33ecc9b8983749e4e39561f5b4aa1ddc3f9daa96
0Lucifer0 <erwan.joly@gmail.com>
2019-08-10 18:41:55 +1200

    add tests

--- test/NosCore.Tests/PacketHandlerTests/MJoinPacketHandlerTests.cs
+++ test/NosCore.Tests/PacketHandlerTests/MJoinPacketHandlerTests.cs
@@ -11,19 +11,20 @@ using NosCore.Core.I18N;
 using NosCore.Data;
 using NosCore.Data.AliveEntities;
 using NosCore.Data.WebApi;
-using NosCore.Database.DAL;
 using NosCore.GameObject;
 using NosCore.GameObject.Networking;
 using NosCore.GameObject.Networking.ClientSession;
-using NosCore.MasterServer.Controllers;
 using NosCore.PacketHandlers.Friend;
 using NosCore.Tests.Helpers;
 using Serilog;
-using NosCore.Core.HttpClients.ChannelHttpClient;
 using NosCore.Core.HttpClients.ConnectedAccountHttpClient;
 using NosCore.GameObject.HttpClients.FriendHttpClient;
-using NosCore.MasterServer.DataHolders;
 using NosCore.GameObject.Providers.MinilandProvider;
+using ChickenAPI.Packets.ClientPackets.Miniland;
+using ChickenAPI.Packets.ServerPackets.UI;
+using NosCore.Data.Enumerations.I18N;
+using NosCore.Data.Enumerations.Character;
+using System.Collections.Generic;
 
 namespace NosCore.Tests.PacketHandlerTests
 {
@@ -50,39 +51,90 @@ namespace NosCore.Tests.PacketHandlerTests
             _minilandProvider = new Mock<IMinilandProvider>();
             _connectedAccountHttpClient = TestHelpers.Instance.ConnectedAccountHttpClient;
             _friendHttpClient = TestHelpers.Instance.FriendHttpClient;
-            _connectedAccountHttpClient.Setup(s => s.GetCharacter(_targetSession.Character.CharacterId, null))
-                .Returns((new ServerConfiguration(), new ConnectedAccount { ChannelId = 1, ConnectedCharacter = new Data.WebApi.Character { Id = _targetSession.Character.CharacterId } }));
-            _connectedAccountHttpClient.Setup(s => s.GetCharacter(_session.Character.CharacterId, null))
-                .Returns((new ServerConfiguration(), new ConnectedAccount { ChannelId = 1, ConnectedCharacter = new Data.WebApi.Character { Id = _session.Character.CharacterId } }));
-
             _mjoinPacketHandler = new MJoinPacketHandler(_friendHttpClient.Object, _minilandProvider.Object);
         }
 
         [TestMethod]
         public void JoinNonConnected()
         {
-         
+            var mjoinPacket = new MJoinPacket
+            {
+                VisualId = 50,
+                Type = VisualType.Player
+            };
+            _mjoinPacketHandler.Execute(mjoinPacket, _session);
+
+            Assert.IsFalse(_session.Character.MapInstance.Map.MapId == 20001);
         }
 
         [TestMethod]
         public void JoinNonFriend()
         {
+            var mjoinPacket = new MJoinPacket
+            {
+                VisualId = _targetSession.Character.CharacterId,
+                Type = VisualType.Player
+            };
+            _mjoinPacketHandler.Execute(mjoinPacket, _session);
 
+            Assert.IsFalse(_session.Character.MapInstance.Map.MapId == 20001);
         }
 
 
         [TestMethod]
         public void JoinClosed()
         {
+            var mjoinPacket = new MJoinPacket
+            {
+                VisualId = _targetSession.Character.CharacterId,
+                Type = VisualType.Player
+            };
+            _connectedAccountHttpClient.Setup(s => s.GetCharacter(_targetSession.Character.CharacterId, null))
+                .Returns((new ServerConfiguration(), new ConnectedAccount { ChannelId = 1, ConnectedCharacter = new Data.WebApi.Character { Id = _targetSession.Character.CharacterId } }));
+            _connectedAccountHttpClient.Setup(s => s.GetCharacter(_session.Character.CharacterId, null))
+                .Returns((new ServerConfiguration(), new ConnectedAccount { ChannelId = 1, ConnectedCharacter = new Data.WebApi.Character { Id = _session.Character.CharacterId } }));
+            _friendHttpClient.Setup(s => s.GetListFriends(It.IsAny<long>())).Returns(new List<CharacterRelationStatus>
+            {
+                new CharacterRelationStatus {
+                    CharacterId = _targetSession.Character.CharacterId,
+                    IsConnected = true,
+                    CharacterName = _targetSession.Character.Name,
+                    RelationType = CharacterRelationType.Friend
+                }
+            });
+            _minilandProvider.Setup(s => s.GetMinilandInfo(It.IsAny<long>())).Returns(new MinilandInfo { MapInstanceId = TestHelpers.Instance.MinilandId, State = MinilandState.Lock });
+            _mjoinPacketHandler.Execute(mjoinPacket, _session);
 
+            var lastpacket = (InfoPacket)_session.LastPackets.FirstOrDefault(s => s is InfoPacket);
+            Assert.AreEqual(lastpacket.Message, Language.Instance.GetMessageFromKey(LanguageKey.MINILAND_CLOSED_BY_FRIEND, _session.Account.Language));
+            Assert.IsFalse(_session.Character.MapInstance.Map.MapId == 20001);
         }
 
         [TestMethod]
         public void Join()
         {
+            var mjoinPacket = new MJoinPacket
+            {
+                VisualId = _targetSession.Character.CharacterId,
+                Type = VisualType.Player
+            };
+            _minilandProvider.Setup(s => s.GetMinilandInfo(It.IsAny<long>())).Returns(new MinilandInfo { MapInstanceId = TestHelpers.Instance.MinilandId, State = MinilandState.Open });
+            _friendHttpClient.Setup(s => s.GetListFriends(It.IsAny<long>())).Returns(new List<CharacterRelationStatus>
+            {
+                new CharacterRelationStatus {
+                    CharacterId = _targetSession.Character.CharacterId,
+                    IsConnected = true,
+                    CharacterName = _targetSession.Character.Name,
+                    RelationType = CharacterRelationType.Friend
+                }
+            });
+            _mjoinPacketHandler.Execute(mjoinPacket, _session);
+            _connectedAccountHttpClient.Setup(s => s.GetCharacter(_targetSession.Character.CharacterId, null))
+                .Returns((new ServerConfiguration(), new ConnectedAccount { ChannelId = 1, ConnectedCharacter = new Data.WebApi.Character { Id = _targetSession.Character.CharacterId } }));
+            _connectedAccountHttpClient.Setup(s => s.GetCharacter(_session.Character.CharacterId, null))
+                .Returns((new ServerConfiguration(), new ConnectedAccount { ChannelId = 1, ConnectedCharacter = new Data.WebApi.Character { Id = _session.Character.CharacterId } }));
 
+            Assert.IsTrue(_session.Character.MapInstance.Map.MapId == 20001);
         }
-
-
     }
 }
