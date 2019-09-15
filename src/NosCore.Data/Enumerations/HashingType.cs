9448d2d932d67333a8a746d9af2c0e1434ccf60d
0Lucifer0 <erwan.joly@gmail.com>
2019-09-16 00:26:35 +1200

    add type to enum

--- src/NosCore.Data/Enumerations/HashingType.cs
+++ src/NosCore.Data/Enumerations/HashingType.cs
@@ -4,6 +4,7 @@
     {
         Sha512,
         //if your care about security use Pbkdf2 (bcrypt doesn't have a approved package for c#)
+        //PBKDF2-HMAC-SHA512-150000
         Pbkdf2,
         BCrypt
     }
