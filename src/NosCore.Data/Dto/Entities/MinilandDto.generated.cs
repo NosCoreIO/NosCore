98449990f8eff91ce1e2903a58d68547a1ba1e92
0Lucifer0 <erwan.joly@gmail.com>
2019-09-29 12:13:49 +1300

    fix dto

--- src/NosCore.Data/Dto/Entities/MinilandDto.generated.cs
+++ src/NosCore.Data/Dto/Entities/MinilandDto.generated.cs
@@ -21,6 +21,7 @@ using System.ComponentModel.DataAnnotations;
 using NosCore.Data.I18N;
 using NosCore.Data.DataAttributes;
 using NosCore.Data.Enumerations.I18N;
+using Mapster;
 
 namespace NosCore.Data.Dto
 {
---
+++ src/NosCore.GameObject/WarehouseItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Data.Dto;
+using NosCore.GameObject.Providers.ItemProvider.Item;
+
+namespace NosCore.GameObject
+{
+    public class WarehouseItem : WarehouseItemDto
+    {
+        public IItemInstance ItemInstance { get; set; }
+    }
+}
