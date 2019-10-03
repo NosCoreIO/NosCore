92a61f866090841dca0ef6da9f62986ed5de3996
0Lucifer0 <erwan.joly@gmail.com>
2019-10-03 20:15:46 +1300

    start httpclients

---
+++ src/NosCore.Data/WebApi/WarehouseLink.cs
@@ -0,0 +1,12 @@
+﻿using NosCore.Data.Dto;
+
+namespace NosCore.Data.WebApi
+{
+    public class WarehouseLink
+    {
+        public WarehouseDto Warehouse { get; set; }
+
+        //todo move this to a generic ItemInstance with a converter
+        public ItemInstanceDto ItemInstance { get; set; }
+    }
+}
