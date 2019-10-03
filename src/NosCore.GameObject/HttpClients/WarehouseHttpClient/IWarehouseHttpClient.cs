b98f6b8a326d9aa3da0e641489c8d2a949cbe3fd
0Lucifer0 <erwan.joly@gmail.com>
2019-10-03 20:15:46 +1300

    start httpclients

--- src/NosCore.GameObject/HttpClients/WarehouseHttpClient/IWarehouseHttpClient.cs
+++ src/NosCore.GameObject/HttpClients/WarehouseHttpClient/IWarehouseHttpClient.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections.Generic;
 using NosCore.Data.Enumerations.Miniland;
+using NosCore.GameObject.Providers.ItemProvider.Item;
 
 namespace NosCore.GameObject.HttpClients.WarehouseHttpClient
 {
     public interface IWarehouseHttpClient
     {
         List<WarehouseItem> GetWarehouseItems(long characterId, WarehouseType warehouse);
+
+        bool DepositItem(long characterCharacterId, WarehouseType warehouse, IItemInstance itemInstance, short slot);
+
+        void DeleteWarehouseItem(long characterId, WarehouseType warehouse, short slot);
+
+        List<WarehouseItem> MoveWarehouseItem(long characterId, WarehouseType warehouse, short slot, short destinationSlot);
+
     }
 }
