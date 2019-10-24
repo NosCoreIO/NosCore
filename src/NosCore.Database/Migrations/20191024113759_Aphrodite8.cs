1bc5e2500bccceef5e938476e98fd9e84de85901
0Lucifer0 <erwan.joly@gmail.com>
2019-10-25 00:38:36 +1300

    add migration

---
+++ src/NosCore.Database/Migrations/20191024113759_Aphrodite8.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NosCore.Database.Migrations
+{
+    public partial class Aphrodite8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateEnd",
+                table: "StaticBonus",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.CreateTable(
+                name: "Title",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CharacterId = table.Column<long>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    Visible = table.Column<bool>(nullable: false),
+                    TitleType = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Title", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Title_Character_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Character",
+                        principalColumn: "CharacterId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Title_CharacterId_TitleType",
+                table: "Title",
+                columns: new[] { "CharacterId", "TitleType" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Title");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateEnd",
+                table: "StaticBonus",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
