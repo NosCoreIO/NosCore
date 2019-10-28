bb5d1a1c1369e83a97cb087bb71454cc492ba734
0Lucifer0 <erwan.joly@gmail.com>
2019-10-28 18:14:13 +1300

    add pcl tests

--- test/NosCore.Tests/ParcelTests/PclPacketHandlerTests.cs
+++ test/NosCore.Tests/ParcelTests/PclPacketHandlerTests.cs
@@ -17,105 +17,128 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using ChickenAPI.Packets.ClientPackets.Parcel;
+using ChickenAPI.Packets.ServerPackets.Parcel;
+using ChickenAPI.Packets.ServerPackets.UI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NosCore.Core;
+using NosCore.Core.HttpClients.ConnectedAccountHttpClient;
+using NosCore.Core.I18N;
+using NosCore.Data;
+using NosCore.Data.CommandPackets;
+using NosCore.Data.Dto;
+using NosCore.Data.Enumerations.I18N;
+using NosCore.Data.WebApi;
+using NosCore.Database;
+using NosCore.GameObject.HttpClients.MailHttpClient;
+using NosCore.GameObject.Networking.ClientSession;
+using NosCore.GameObject.Providers.ItemProvider;
+using NosCore.PacketHandlers.Command;
+using NosCore.PacketHandlers.Inventory;
+using NosCore.PacketHandlers.Parcel;
+using NosCore.Tests.Helpers;
+using Serilog;
 
 namespace NosCore.Tests.ParcelTests
 {
     [TestClass]
     public class PclPacketHandlerTests
     {
+        private static readonly ILogger _logger = Logger.GetLoggerConfiguration().CreateLogger();
+        private Mock<IMailHttpClient> _mailHttpClient;
+        private PclPacketHandler _pclPacketHandler;
+        private IItemProvider _item;
+        private ClientSession _session;
+        private Mock<IGenericDao<IItemInstanceDto>> _itemInstanceDao;
+
         [TestInitialize]
         public void Setup()
         {
+            SystemTime.Freeze();
+            _session = TestHelpers.Instance.GenerateSession();
+            _item = TestHelpers.Instance.GenerateItemProvider();
+            _mailHttpClient = new Mock<IMailHttpClient>();
+            _itemInstanceDao = new Mock<IGenericDao<IItemInstanceDto>>();
+            _pclPacketHandler = new PclPacketHandler(_mailHttpClient.Object, _item, _itemInstanceDao.Object);
         }
 
         [TestMethod]
         public void Test_GiftNotFound()
         {
-            Assert.Fail();
+            _mailHttpClient.Setup(s => s.GetGift(1, _session.Character.CharacterId, false)).Returns((MailData)null);
+            _pclPacketHandler.Execute(new PclPacket
+            {
+                Type = 5,
+                GiftId = 1
+            }, _session);
+            var packet = (ParcelPacket)_session.LastPackets.FirstOrDefault(s => s is ParcelPacket);
+            Assert.IsNull(packet);
         }
 
         [TestMethod]
         public void Test_DeleteGift()
         {
-            //else if (getGiftPacket.Type == 5)
-            //{
-            //    clientSession.SendPacket(new ParcelPacket { Type = 7, Unknown = 1, Id = (short)getGiftPacket.GiftId });
-            //    _mailHttpClient.DeleteGift(getGiftPacket.GiftId, clientSession.Character.VisualId, isCopy);
-            //}
-            Assert.Fail();
+            _mailHttpClient.Setup(s => s.GetGift(1, _session.Character.CharacterId, false)).Returns(new MailData());
+            _pclPacketHandler.Execute(new PclPacket
+            {
+                Type = 5,
+                GiftId = 1
+            }, _session);
+            var packet = (ParcelPacket)_session.LastPackets.FirstOrDefault(s => s is ParcelPacket);
+            Assert.IsTrue(packet.Type == 7);
         }
 
         [TestMethod]
         public void Test_ReceiveGift()
         {
-            //if ((getGiftPacket.Type == 4) && (mail.ItemInstance != null))
-            //{
-            //    var itemInstance = _itemInstanceDao.FirstOrDefault(s => s.Id == mail.ItemInstance.Id);
-            //    var item = _itemProvider.Convert(itemInstance);
-            //    item.Id = Guid.NewGuid();
-            //    var newInv = clientSession.Character.Inventory
-            //        .AddItemToPocket(InventoryItemInstance.Create(item, clientSession.Character.CharacterId))
-            //        .FirstOrDefault();
-            //    if (newInv != null)
-            //    {
-            //        clientSession.SendPacket(clientSession.Character.GenerateSay(
-            //            string.Format(
-            //                Language.Instance.GetMessageFromKey(LanguageKey.ITEM_RECEIVED,
-            //                    clientSession.Account.Language),
-            //                newInv.ItemInstance.Item.Name, newInv.ItemInstance.Amount), SayColorType.Green));
-            //        clientSession.SendPacket(
-            //            new ParcelPacket { Type = 2, Unknown = 1, Id = (short)getGiftPacket.GiftId });
-            //        _mailHttpClient.DeleteGift(getGiftPacket.GiftId, clientSession.Character.VisualId, isCopy);
-            //    }
-            //    else
-            //    {
-            //        clientSession.SendPacket(new ParcelPacket { Type = 5, Unknown = 1, Id = 0 });
-            //        clientSession.SendPacket(new MsgPacket
-            //        {
-            //            Message = Language.Instance.GetMessageFromKey(LanguageKey.NOT_ENOUGH_PLACE,
-            //                clientSession.Account.Language),
-            //            Type = 0
-            //        });
-            //    }
-            //}
-            Assert.Fail();
+            var item = _item.Create(1);
+            var mail = new MailData
+            {
+                ItemInstance = (ItemInstanceDto) item,
+                MailDto = new MailDto
+                {
+                    ItemInstanceId = item.Id
+                }
+            };
+            _itemInstanceDao.Setup(o => o.FirstOrDefault(It.IsAny<Expression<Func<IItemInstanceDto, bool>>>()))
+                .Returns(item);
+            _mailHttpClient.Setup(s => s.GetGift(1, _session.Character.CharacterId, false)).Returns(mail);
+            _pclPacketHandler.Execute(new PclPacket
+            {
+                Type = 4,
+                GiftId = 1
+            }, _session);
+            var packet = (ParcelPacket)_session.LastPackets.FirstOrDefault(s => s is ParcelPacket);
+            Assert.IsTrue(packet.Type == 2);
         }
 
         [TestMethod]
         public void Test_ReceiveGiftNoPlace()
         {
-            //if ((getGiftPacket.Type == 4) && (mail.ItemInstance != null))
-            //{
-            //    var itemInstance = _itemInstanceDao.FirstOrDefault(s => s.Id == mail.ItemInstance.Id);
-            //    var item = _itemProvider.Convert(itemInstance);
-            //    item.Id = Guid.NewGuid();
-            //    var newInv = clientSession.Character.Inventory
-            //        .AddItemToPocket(InventoryItemInstance.Create(item, clientSession.Character.CharacterId))
-            //        .FirstOrDefault();
-            //    if (newInv != null)
-            //    {
-            //        clientSession.SendPacket(clientSession.Character.GenerateSay(
-            //            string.Format(
-            //                Language.Instance.GetMessageFromKey(LanguageKey.ITEM_RECEIVED,
-            //                    clientSession.Account.Language),
-            //                newInv.ItemInstance.Item.Name, newInv.ItemInstance.Amount), SayColorType.Green));
-            //        clientSession.SendPacket(
-            //            new ParcelPacket { Type = 2, Unknown = 1, Id = (short)getGiftPacket.GiftId });
-            //        _mailHttpClient.DeleteGift(getGiftPacket.GiftId, clientSession.Character.VisualId, isCopy);
-            //    }
-            //    else
-            //    {
-            //        clientSession.SendPacket(new ParcelPacket { Type = 5, Unknown = 1, Id = 0 });
-            //        clientSession.SendPacket(new MsgPacket
-            //        {
-            //            Message = Language.Instance.GetMessageFromKey(LanguageKey.NOT_ENOUGH_PLACE,
-            //                clientSession.Account.Language),
-            //            Type = 0
-            //        });
-            //    }
-            //}
-            Assert.Fail();
+            TestHelpers.Instance.WorldConfiguration.BackpackSize = 0;
+            var item = _item.Create(1);
+            var mail = new MailData
+            {
+                ItemInstance = (ItemInstanceDto)item,
+                MailDto = new MailDto
+                {
+                    ItemInstanceId = item.Id
+                }
+            };
+            _itemInstanceDao.Setup(o => o.FirstOrDefault(It.IsAny<Expression<Func<IItemInstanceDto, bool>>>()))
+                .Returns(item);
+            _mailHttpClient.Setup(s => s.GetGift(1, _session.Character.CharacterId, false)).Returns(mail);
+            _pclPacketHandler.Execute(new PclPacket
+            {
+                Type = 4,
+                GiftId = 1
+            }, _session);
+            var packet = (ParcelPacket)_session.LastPackets.FirstOrDefault(s => s is ParcelPacket);
+            Assert.IsTrue(packet.Type == 5);
         }
     }
 }
