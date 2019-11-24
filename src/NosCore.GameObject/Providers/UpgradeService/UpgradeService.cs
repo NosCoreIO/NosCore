7b5a3c4c098f504c5fe292ea90cff99122efeeda
Unknown <alex.c9777700@gmail.com>
2019-11-24 12:07:23 +0100

    Add header and remove unused "using"

--- src/NosCore.GameObject/Providers/UpgradeService/UpgradeService.cs
+++ src/NosCore.GameObject/Providers/UpgradeService/UpgradeService.cs
@@ -1,6 +1,24 @@
-﻿using System;
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// 
+// Copyright (C) 2019 - NosCore
+// 
+// NosCore is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
-using System.Text;
 using ChickenAPI.Packets.ClientPackets.Inventory;
 using ChickenAPI.Packets.Enumerations;
 using ChickenAPI.Packets.Interfaces;
@@ -14,23 +32,19 @@ using NosCore.GameObject.ComponentEntities.Extensions;
 using NosCore.GameObject.Helper;
 using NosCore.GameObject.Networking.ClientSession;
 using NosCore.GameObject.Providers.InventoryService;
-using NosCore.GameObject.Providers.ItemProvider.Item;
-using Serilog;
 
 namespace NosCore.GameObject.Providers.UpgradeService
 {
     public class UpgradeService: IUpgradeService
     {
-        private readonly ILogger _logger;
         private readonly Dictionary<UpgradePacketType,
             Func<ClientSession, InventoryItemInstance, InventoryItemInstance, InventoryItemInstance>> PacketsFunctions;
 
-        public UpgradeService(ILogger logger)
+        public UpgradeService()
         {
-            _logger = logger;
             PacketsFunctions = new Dictionary<UpgradePacketType, Func<ClientSession, InventoryItemInstance, InventoryItemInstance, InventoryItemInstance>>
                 {
-                    { UpgradePacketType.SumResistance, new Func<ClientSession, InventoryItemInstance, InventoryItemInstance, InventoryItemInstance>(Sum) }
+                    { UpgradePacketType.SumResistance, Sum }
                 };
         }
 
