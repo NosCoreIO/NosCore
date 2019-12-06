b8a77fbfb1ce82bbfbed05f4b486c1272c50d35a
0Lucifer0 <erwan.joly@gmail.com>
2019-12-06 21:50:24 +1300

    start adding speakers

--- src/NosCore.Data/Enumerations/Items/ItemEffectType.cs
+++ src/NosCore.Data/Enumerations/Items/ItemEffectType.cs
@@ -24,6 +24,7 @@ namespace NosCore.Data.Enumerations.Items
         NoEffect = 0,
         Teleport = 1,
         ApplyHairDie = 10,
+        Speaker= 15,
         MarriageProposal = 34,
         Undefined = 69,
         SpCharger = 71,
