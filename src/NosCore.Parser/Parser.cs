bca9b0fbe6e3074a1c0a00748f88e7c0a5eb4284
erwan-joly <erwan.joly@pushpay.com>
2020-04-09 21:47:19 +1200

    fix

--- src/NosCore.Parser/Parser.cs
+++ src/NosCore.Parser/Parser.cs
@@ -135,7 +135,7 @@ namespace NosCore.Parser
                             var type = assemblyDb.First(tgo =>
                                 string.Compare(t.Name, $"{tgo.Name}Dto", StringComparison.OrdinalIgnoreCase) == 0);
                             var typepk = type.GetProperties()
-                                .Where(s => dataAccess.CreateContext().Model.FindEntityType(t)
+                                .Where(s => dataAccess.CreateContext().Model.FindEntityType(type)
                                     .FindPrimaryKey().Properties.Select(x => x.Name)
                                     .Contains(s.Name)
                                 ).ToArray()[0];
