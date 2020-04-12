dc7fc83c62c8f626504e6b6bfb3afc2ba14a60b1
0Lucifer0 <erwan.joly@gmail.com>
2020-04-12 14:28:58 +1200

    add exceptions to messages

--- src/NosCore.Database/Migrations/NosCoreContextModelSnapshot.cs
+++ src/NosCore.Database/Migrations/NosCoreContextModelSnapshot.cs
@@ -2,6 +2,8 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using NosCore.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace NosCore.Database.Migrations
