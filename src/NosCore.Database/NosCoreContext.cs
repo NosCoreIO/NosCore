a75671ad7fe3fc8e917db852c8f339bdb0564d4b
0Lucifer0 <erwan.joly@gmail.com>
2020-04-13 15:09:45 +1200

    cleanup

--- src/NosCore.Database/NosCoreContext.cs
+++ src/NosCore.Database/NosCoreContext.cs
@@ -18,6 +18,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using Microsoft.EntityFrameworkCore;
+using NosCore.Dao.Extensions;
 using NosCore.Database.Entities;
 
 namespace NosCore.Database
