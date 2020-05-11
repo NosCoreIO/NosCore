64525c2b17d89028ae4514ef8523fbd1b297eba1
erwan-joly <erwan.joly@pushpay.com>
2020-05-11 22:04:40 +1200

    start fixing docker

--- src/NosCore.Core/Configuration/Configurator.cs
+++ src/NosCore.Core/Configuration/Configurator.cs
@@ -1,34 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 
 namespace NosCore.Core.Configuration
 {
     public static class Configurator
     {
-        public static void Configure(IConfiguration configuration, object strongTypedConfiguration)
+        private static IConfiguration ReplaceEnvironment(IConfiguration configuration, object strongTypedConfiguration)
         {
-            var regexp = new Regex(@"\${(?<variable>[a-zA-Z_]+)\s+,?\s+(?<fallback>[^}]+)}");
-            if (configuration != null)
+            var ses = configuration.GetChildren().ToList();
+            for (var index = ses.Count; index > 0; index--)
             {
-                foreach (var s in configuration.GetChildren().Where(s => s.Value != null).ToList())
+                if (ses[index - 1].Value != null)
                 {
-                    var matches = regexp.Matches(s.Value);
+                    var regexp = new Regex(@"\${(?<variable>[a-zA-Z_]+)\s*,?\s*(?<fallback>[^}]+)}");
+                    var matches = regexp.Matches(ses[index - 1].Value);
                     foreach (var match in matches.ToList())
                     {
                         var value = Environment.GetEnvironmentVariable(match.Groups[1].Value);
-                        if (value == null && (match.Groups.Count > 2) && (match.Groups[2].Value != null))
+                        if (string.IsNullOrEmpty(value) && (match.Groups.Count > 2) && (match.Groups[2].Value != null))
                         {
                             value ??= match.Groups[2].Value;
                         }
-
-                        s.Value = regexp.Replace(s.Value, value!, 1);
+                        ses[index - 1].Value = regexp.Replace(ses[index - 1].Value, value!, 1);
                     }
                 }
+                else if (ses[index - 1].GetChildren().Any())
+                {
+                    ReplaceEnvironment(configuration.GetSection(ses[index - 1].Path), configuration).Bind(strongTypedConfiguration);
+                }
+            }
 
-                configuration.Bind(strongTypedConfiguration);
+            return configuration;
+        }
+        public static void Configure(IConfiguration configuration, object strongTypedConfiguration)
+        {
+            if (configuration != null)
+            {
+                ReplaceEnvironment(configuration, strongTypedConfiguration).Bind(strongTypedConfiguration);
             }
 
             Validator.ValidateObject(strongTypedConfiguration, new ValidationContext(strongTypedConfiguration), true);
