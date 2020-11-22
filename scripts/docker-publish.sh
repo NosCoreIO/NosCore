081f27c58dc56344a46bd94ca37fe03f4ab9c7b3
Erwan JOLY <35202750+erwan-joly@users.noreply.github.com>
2020-11-22 15:21:37 +1300

    Update docker-publish.sh

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+dotnet test --runtime linux-musl-x64 --filter TestCategory=SomethingAsDotnetBuildDoesntWorkAnymore
 
 DOCKER_ENV=''
 DOCKER_TAG=''
