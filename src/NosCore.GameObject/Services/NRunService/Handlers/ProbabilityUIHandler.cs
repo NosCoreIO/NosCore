b053748ef8958211ba9a597b920c2af2be625d71
Bari <alex.c9777700@gmail.com>
2021-06-27 10:39:16 +0200

    Add: ProbabilityUIHandler

---
+++ src/NosCore.GameObject/Services/NRunService/Handlers/ProbabilityUIHandler.cs
@@ -0,0 +1,40 @@
+﻿using NosCore.GameObject.ComponentEntities.Interfaces;
+using NosCore.GameObject.Networking.ClientSession;
+using NosCore.Packets.ClientPackets.Npcs;
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.ServerPackets.UI;
+using System;
+using System.Threading.Tasks;
+
+namespace NosCore.GameObject.Services.NRunService.Handlers
+{
+    /// <summary>
+    /// Handler for ProbabilityUIs nrun type
+    /// </summary>
+    public class ProbabilityUIHandler : INrunEventHandler
+    {
+        /// <summary>
+        /// Conditions to execute packet sending
+        /// </summary>
+        /// <param name="datas">Data of request</param>
+        /// <returns>true if condition is respected else false</returns>
+        public bool Condition(Tuple<IAliveEntity, NrunPacket> datas)
+        {
+            return datas.Item2.Runner == NrunRunnerType.ProbabilityUIs;
+        }
+
+        /// <summary>
+        /// Method called when event will be handled
+        /// </summary>
+        /// <param name="requestData">Data of request</param>
+        /// <returns>Task to execute</returns>
+        public Task ExecuteAsync(RequestData<Tuple<IAliveEntity, NrunPacket>> requestData)
+        {
+            return requestData.ClientSession.SendPacketAsync(
+                new WopenPacket
+                {
+                    Type = (WindowType)(requestData.Data.Item2.Type ?? 0)
+                });
+        }
+    }
+}
