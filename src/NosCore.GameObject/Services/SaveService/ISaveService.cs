5b673c34acaa33cc7e3335020d85d66ae143a1a9
erwan-joly <erwan.joly@pushpay.com>
2022-01-05 17:43:14 +1300

    add save and speed calculation services

---
+++ src/NosCore.GameObject/Services/SaveService/ISaveService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NosCore.GameObject.ComponentEntities.Interfaces;
+
+namespace NosCore.GameObject.Services.SaveService
+{
+    public interface ISaveService
+    {
+        Task SaveAsync(ICharacterEntity character);
+    }
+}
